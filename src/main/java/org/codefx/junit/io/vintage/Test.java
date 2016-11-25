d6ec1baba4924334faa86560f9b612905084ae1b
nipa <nipa@codefx.org>
2016-11-25 18:18:16 +0100

    Implement expected exception (#7)

--- src/main/java/org/codefx/junit/io/vintage/Test.java
+++ src/main/java/org/codefx/junit/io/vintage/Test.java
@@ -15,9 +15,26 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+
 @Target({ ElementType.ANNOTATION_TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(ExpectedExceptionExtension.class)
 @org.junit.jupiter.api.Test
 public @interface Test {
+
+	class None extends Throwable {
+
+		private static final long serialVersionUID = 1L;
+
+		private None() {
+		}
+	}
+
+	/**
+	 * Optionally specify <code>expected</code>, a Throwable, to cause a test method to succeed if
+	 * and only if an exception of the specified class is thrown by the method.
+	 */
+	Class<? extends Throwable> expected() default None.class;
 
 }
--- src/test/java/org/codefx/junit/io/vintage/TestIntegrationTest.java
+++ src/test/java/org/codefx/junit/io/vintage/TestIntegrationTest.java
@@ -10,10 +10,16 @@
 
 package org.codefx.junit.io.vintage;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static java.lang.String.format;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.codefx.junit.io.vintage.ExpectedExceptionExtension.EXPECTED_EXCEPTION_WAS_NOT_THROWN;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.nio.file.InvalidPathException;
+import java.util.Optional;
+
 import org.codefx.junit.io.AbstractIoTestEngineTests;
+import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.engine.test.event.ExecutionEventRecorder;
 
 public class TestIntegrationTest extends AbstractIoTestEngineTests {
@@ -22,16 +28,70 @@ public class TestIntegrationTest extends AbstractIoTestEngineTests {
 	void test_successfulTest_passes() throws Exception {
 		ExecutionEventRecorder eventRecorder = executeTests(TestTestCase.class, "test_successfulTest");
 
-		assertEquals(1, eventRecorder.getTestStartedCount(), "# tests started");
-		assertEquals(1, eventRecorder.getTestSuccessfulCount(), "# tests succeeded");
+		assertThat(eventRecorder.getTestStartedCount()).isEqualTo(1);
+		assertThat(eventRecorder.getTestSuccessfulCount()).isEqualTo(1);
 	}
 
 	@org.junit.jupiter.api.Test
 	void test_exceptionThrown_fails() throws Exception {
 		ExecutionEventRecorder eventRecorder = executeTests(TestTestCase.class, "test_exceptionThrown");
 
-		assertEquals(1, eventRecorder.getTestStartedCount(), "# tests started");
-		assertEquals(1, eventRecorder.getTestFailedCount(), "# tests failed");
+		assertThat(eventRecorder.getTestStartedCount()).isEqualTo(1);
+		assertThat(eventRecorder.getTestFailedCount()).isEqualTo(1);
+	}
+
+	@org.junit.jupiter.api.Test
+	void testWithExpectedException_successfulTest_fails() {
+		ExecutionEventRecorder eventRecorder = executeTests(TestTestCase.class,
+			"testWithExpectedException_successfulTest");
+
+		assertThat(eventRecorder.getTestStartedCount()).isEqualTo(1);
+		assertThat(eventRecorder.getTestFailedCount()).isEqualTo(1);
+
+		//@formatter:off
+		Optional<String> failedTestMessage = eventRecorder
+				.getFailedTestFinishedEvents().get(0)
+				.getPayload(TestExecutionResult.class)
+				.flatMap(TestExecutionResult::getThrowable)
+				.map(Throwable::getMessage);
+		//@formatter:on
+		String expectedMessage = format(EXPECTED_EXCEPTION_WAS_NOT_THROWN, IllegalArgumentException.class);
+		assertThat(failedTestMessage).contains(expectedMessage);
+	}
+
+	@org.junit.jupiter.api.Test
+	void testWithExpectedException_exceptionThrownOfRightType_passes() {
+		ExecutionEventRecorder eventRecorder = executeTests(TestTestCase.class,
+			"testWithExpectedException_exceptionThrownOfRightType");
+
+		assertThat(eventRecorder.getTestStartedCount()).isEqualTo(1);
+		assertThat(eventRecorder.getTestSuccessfulCount()).isEqualTo(1);
+	}
+
+	@org.junit.jupiter.api.Test
+	void testWithExpectedException_exceptionThrownOfSubtype_passes() {
+		ExecutionEventRecorder eventRecorder = executeTests(TestTestCase.class,
+			"testWithExpectedException_exceptionThrownOfSubtype");
+
+		assertThat(eventRecorder.getTestStartedCount()).isEqualTo(1);
+		assertThat(eventRecorder.getTestSuccessfulCount()).isEqualTo(1);
+	}
+
+	@org.junit.jupiter.api.Test
+	void testWithExpectedException_exceptionThrownOfSupertype_fails() {
+		ExecutionEventRecorder eventRecorder = executeTests(TestTestCase.class,
+			"testWithExpectedException_exceptionThrownOfSupertype");
+
+		assertThat(eventRecorder.getTestStartedCount()).isEqualTo(1);
+		assertThat(eventRecorder.getTestFailedCount()).isEqualTo(1);
+
+		//@formatter:off
+		Optional<Throwable> failedTestThrowable = eventRecorder
+				.getFailedTestFinishedEvents().get(0)
+				.getPayload(TestExecutionResult.class)
+				.flatMap(TestExecutionResult::getThrowable);
+		//@formatter:on
+		assertThat(failedTestThrowable).containsInstanceOf(RuntimeException.class);
 	}
 
 	// TEST CASES -------------------------------------------------------------------
@@ -48,9 +108,24 @@ public class TestIntegrationTest extends AbstractIoTestEngineTests {
 			throw new IllegalArgumentException();
 		}
 
-		@Test
+		@Test(expected = IllegalArgumentException.class)
 		void testWithExpectedException_successfulTest() {
 			assertTrue(true);
+		}
+
+		@Test(expected = IllegalArgumentException.class)
+		void testWithExpectedException_exceptionThrownOfRightType() {
+			throw new IllegalArgumentException();
+		}
+
+		@Test(expected = IllegalArgumentException.class)
+		void testWithExpectedException_exceptionThrownOfSubtype() {
+			throw new InvalidPathException("", "");
+		}
+
+		@Test(expected = IllegalArgumentException.class)
+		void testWithExpectedException_exceptionThrownOfSupertype() {
+			throw new RuntimeException();
 		}
 
 	}
