edc3e9cd3ee5f33c0e6a341f9c7b7efcedc60a6e
Benedikt Ritter <beneritter@gmail.com>
2018-09-10 16:11:12 +0200

    Add documentation

--- src/main/java/org/junitpioneer/jupiter/DefaultLocale.java
+++ src/main/java/org/junitpioneer/jupiter/DefaultLocale.java
@@ -17,6 +17,26 @@ import java.lang.annotation.Target;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 
+/**
+ * {@code @DefaultLocale} is a JUnit Jupiter extension to change the value
+ * returned by {@link java.util.Locale#getDefault()} for a test execution.
+ *
+ * <p>The {@link java.util.Locale} to set as the default locale can be
+ * configured using only a language, a language and a country or a language, a
+ * country and a variant. If {@link #variant()} is set, but {@link #country()}
+ * is not, a
+ * {@link org.junit.jupiter.api.extension.ExtensionConfigurationException} will
+ * be thrown. After the annotated element has been executed, the default Locale
+ * will be restored to its original value.</p>
+ *
+ * <p>{@code @DefaultLocale} can be used on the method and on the class level.
+ * If a class is annotated, the configured Locale will be the default Locale
+ * for all tests inside that class. Any method level configurations will
+ * override the class level default Locale.</p>
+ *
+ * @since 0.2
+ * @see java.util.Locale#getDefault()
+ */
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @ExtendWith(DefaultLocaleExtension.class)
