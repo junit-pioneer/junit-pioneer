7c793e9441f1496153e581e9a294605b64048c7c
Nicolai Parlog <nipa@codefx.org>
2018-09-21 23:29:37 +0200

    Turn demo into tests

--- src/test/java/org/junitpioneer/jupiter/RepeatFailedTestTests.java
+++ src/test/java/org/junitpioneer/jupiter/RepeatFailedTestTests.java
@@ -1,23 +1,61 @@
 package org.junitpioneer.jupiter;
 
-class RepeatFailedTestTests {
+import org.junit.jupiter.api.Test;
+import org.junit.platform.engine.test.event.ExecutionEventRecorder;
+import org.junitpioneer.AbstractPioneerTestEngineTests;
 
-	private static int FAILS_ONLY_ON_FIRST_INVOCATION;
+import static org.assertj.core.api.Assertions.assertThat;
 
-	@RepeatFailedTest(3)
-	void failsNever_executedOnce_passes() { }
+class RepeatFailedTestTests extends AbstractPioneerTestEngineTests {
 
-	@RepeatFailedTest(3)
-	void failsOnlyOnFirstInvocation_executedTwice_passes() {
-		FAILS_ONLY_ON_FIRST_INVOCATION++;
-		if (FAILS_ONLY_ON_FIRST_INVOCATION == 1) {
-			throw new IllegalArgumentException();
-		}
+	@Test
+	void failsNever_executedOnce_passes() throws Exception {
+		ExecutionEventRecorder eventRecorder = executeTests(RepeatFailedTestTestCase.class, "failsNever");
+
+		assertThat(eventRecorder.getDynamicTestRegisteredCount()).isEqualTo(1);
+		assertThat(eventRecorder.getTestSuccessfulCount()).isEqualTo(1);
 	}
 
-	@RepeatFailedTest(3)
-	void failsAlways_executedThreeTimes_fails() {
-		throw new IllegalArgumentException();
+	@Test
+	void failsOnlyOnFirstInvocation_executedTwice_passes() throws Exception {
+		ExecutionEventRecorder eventRecorder = executeTests(RepeatFailedTestTestCase.class, "failsOnlyOnFirstInvocation");
+
+		assertThat(eventRecorder.getDynamicTestRegisteredCount()).isEqualTo(2);
+		assertThat(eventRecorder.getTestSuccessfulCount()).isEqualTo(2);
+	}
+
+	@Test
+	void failsAlways_executedThreeTimes_fails() throws Exception {
+		ExecutionEventRecorder eventRecorder = executeTests(RepeatFailedTestTestCase.class, "failsAlways");
+
+		assertThat(eventRecorder.getDynamicTestRegisteredCount()).isEqualTo(3);
+		assertThat(eventRecorder.getTestSuccessfulCount()).isEqualTo(2);
+		assertThat(eventRecorder.getTestFailedCount()).isEqualTo(1);
+	}
+
+	// TEST CASES -------------------------------------------------------------------
+
+	static class RepeatFailedTestTestCase {
+
+		private static int FAILS_ONLY_ON_FIRST_INVOCATION;
+
+		@RepeatFailedTest(3)
+		void failsNever() {
+		}
+
+		@RepeatFailedTest(3)
+		void failsOnlyOnFirstInvocation() {
+			FAILS_ONLY_ON_FIRST_INVOCATION++;
+			if (FAILS_ONLY_ON_FIRST_INVOCATION == 1) {
+				throw new IllegalArgumentException();
+			}
+		}
+
+		@RepeatFailedTest(3)
+		void failsAlways() {
+			throw new IllegalArgumentException();
+		}
+
 	}
 
 }
