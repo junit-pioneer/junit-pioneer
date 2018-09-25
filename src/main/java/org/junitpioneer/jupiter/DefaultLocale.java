4f4f0c67d9928cdb69dd785a42e6c22294172b4b
Benedikt Ritter <beneritter@gmail.com>
2018-09-25 16:40:12 +0200

    Extend JavaDoc of DefaultLocale as suggested by @nicolaiparlog

--- src/main/java/org/junitpioneer/jupiter/DefaultLocale.java
+++ src/main/java/org/junitpioneer/jupiter/DefaultLocale.java
@@ -22,17 +22,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
  * returned by {@link java.util.Locale#getDefault()} for a test execution.
  *
  * <p>The {@link java.util.Locale} to set as the default locale can be
- * configured using only a language, a language and a country or a language, a
+ * configured using only a language, a language and a country, or a language, a
  * country and a variant. If {@link #variant()} is set, but {@link #country()}
  * is not, a
  * {@link org.junit.jupiter.api.extension.ExtensionConfigurationException} will
- * be thrown. After the annotated element has been executed, the default Locale
- * will be restored to its original value.</p>
+ * be thrown. After the annotated element has been executed, the default
+ * {@code Locale} will be restored to its original value.</p>
  *
  * <p>{@code @DefaultLocale} can be used on the method and on the class level.
- * If a class is annotated, the configured Locale will be the default Locale
- * for all tests inside that class. Any method level configurations will
- * override the class level default Locale.</p>
+ * If a class is annotated, the configured {@code Locale} will be the default
+ * {@code Locale} for all tests inside that class. Any method level
+ * configurations will override the class level default {@code Locale}.</p>
  *
  * @since 0.2
  * @see java.util.Locale#getDefault()
@@ -43,9 +43,23 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @ExtendWith(DefaultLocaleExtension.class)
 public @interface DefaultLocale {
 
+	/**
+	 * An ISO 639 alpha-2 or alpha-3 language code, or a language subtag up to
+	 * 8 characters in length. See the {@link java.util.Locale} class
+	 * description about valid language values.
+	 */
 	String language();
 
+	/**
+	 * An ISO 3166 alpha-2 country code or a UN M.49 numeric-3 area code. See
+	 * the {@link java.util.Locale} class description about valid country
+	 * values.
+	 */
 	String country() default "";
 
+	/**
+	 * Any arbitrary value used to indicate a variation of a {@code Locale}.
+	 * See the {@link java.util.Locale} class description for the details.
+	 */
 	String variant() default "";
 }
