da27b46a5779a56d2c33b14e6bff5168e3251974
Benedikt Ritter <beneritter@gmail.com>
2018-09-26 15:21:51 +0200

    Rewrite DefaultLocaleTests

--- src/test/java/org/junitpioneer/jupiter/DefaultLocaleTests.java
+++ src/test/java/org/junitpioneer/jupiter/DefaultLocaleTests.java
@@ -15,13 +15,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Locale;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.engine.test.event.ExecutionEventRecorder;
+import org.junitpioneer.AbstractPioneerTestEngineTests;
 
 @DisplayName("DefaultLocale extension")
-class DefaultLocaleTests {
+class DefaultLocaleTests extends AbstractPioneerTestEngineTests {
 
 	private static Locale TEST_DEFAULT_LOCALE;
 	private static Locale DEFAULT_LOCALE_BEFORE_TEST;
@@ -29,53 +33,120 @@ class DefaultLocaleTests {
 	@BeforeAll
 	static void globalSetUp() {
 		DEFAULT_LOCALE_BEFORE_TEST = Locale.getDefault();
-		if (!DEFAULT_LOCALE_BEFORE_TEST.equals(Locale.CANADA)) {
-			Locale.setDefault(Locale.CANADA);
-		}
-		else {
-			// you seem to be from Canada...
-			Locale.setDefault(Locale.CHINESE);
-		}
-		TEST_DEFAULT_LOCALE = Locale.getDefault();
-	}
-
-	@Test
-	@DisplayName("does nothing when annotation is not present")
-	void testDefaultLocaleNoAnnotation() {
-		assertEquals(TEST_DEFAULT_LOCALE, Locale.getDefault());
-	}
-
-	@DefaultLocale(language = "en_EN")
-	@Test
-	@DisplayName("sets the default locale using a language")
-	void setsLanguage() {
-		assertEquals(new Locale("en_EN"), Locale.getDefault());
-	}
-
-	@DefaultLocale(language = "en", country = "EN")
-	@Test
-	@DisplayName("sets the default locale using a language and a country")
-	void setsLanguageAndCountry() {
-		assertEquals(new Locale("en", "EN"), Locale.getDefault());
-	}
-
-	@DefaultLocale(language = "en", country = "EN", variant = "gb")
-	@Test
-	@DisplayName("sets the default locale using a language, a country and a variant")
-	void setsLanguageAndCountryAndVariant() {
-		assertEquals(new Locale("en", "EN", "gb"), Locale.getDefault());
-	}
-
-	@Disabled("TODO: How to test this?")
-	@DefaultLocale(language = "en", variant = "gb")
-	@Test
-	@DisplayName("fails when variant is set but country is not")
-	void failsWhenVariantIsSetButCountryIsNotSet() {
-		assertEquals(new Locale("en_EN"), Locale.getDefault());
+		TEST_DEFAULT_LOCALE = new Locale("custom");
+		Locale.setDefault(TEST_DEFAULT_LOCALE);
 	}
 
 	@AfterAll
 	static void globalTearDown() {
 		Locale.setDefault(DEFAULT_LOCALE_BEFORE_TEST);
 	}
+
+	@Nested
+	@DisplayName("applied on the method level")
+	class MethodLevelTests {
+
+		@Test
+		@DisplayName("does nothing when annotation is not present")
+		void testDefaultLocaleNoAnnotation() {
+			assertEquals(TEST_DEFAULT_LOCALE, Locale.getDefault());
+		}
+
+		@DefaultLocale(language = "en_EN")
+		@Test
+		@DisplayName("sets the default locale using a language")
+		void setsLanguage() {
+			assertEquals(new Locale("en_EN"), Locale.getDefault());
+		}
+
+		@DefaultLocale(language = "en", country = "EN")
+		@Test
+		@DisplayName("sets the default locale using a language and a country")
+		void setsLanguageAndCountry() {
+			assertEquals(new Locale("en", "EN"), Locale.getDefault());
+		}
+
+		@DefaultLocale(language = "en", country = "EN", variant = "gb")
+		@Test
+		@DisplayName("sets the default locale using a language, a country and a variant")
+		void setsLanguageAndCountryAndVariant() {
+			assertEquals(new Locale("en", "EN", "gb"), Locale.getDefault());
+		}
+	}
+
+	@Nested
+	@DisplayName("applied on the class level")
+	class ClassLevelTests {
+
+		@BeforeEach
+		void setUp() {
+			assertEquals(TEST_DEFAULT_LOCALE, Locale.getDefault());
+		}
+
+		@Test
+		@DisplayName("should execute tests with configured Locale")
+		void shouldExecuteTestsWithConfiguredLocale() {
+			ExecutionEventRecorder eventRecorder = executeTestsForClass(ClassLevelTestCase.class);
+
+			assertEquals(2, eventRecorder.getTestSuccessfulCount());
+		}
+
+		@AfterEach
+		void tearDown() {
+			assertEquals(TEST_DEFAULT_LOCALE, Locale.getDefault());
+		}
+	}
+
+	@DefaultLocale(language = "fr", country = "FR")
+	static class ClassLevelTestCase {
+
+		@Test
+		void shouldExecuteWithClassLevelLocale() {
+			assertEquals(new Locale("fr", "FR"), Locale.getDefault());
+		}
+
+		@Test
+		@DefaultLocale(language = "de", country = "DE")
+		void shouldBeOverriddenWithMethodLevelLocale() {
+			assertEquals(new Locale("de", "DE"), Locale.getDefault());
+		}
+	}
+
+	@Nested
+	@DisplayName("when configured incorrect")
+	class ConfigurationFailureTests {
+
+		@Test
+		@DisplayName("should fail when variant is set but country is not on method level")
+		void shouldFailWhenVariantIsSetButCountryIsNotOnMethodLevel() {
+			ExecutionEventRecorder eventRecorder = executeTestsForClass(MethodLevelInitializationFailureTestCase.class);
+
+			assertEquals(1, eventRecorder.getTestFailedCount());
+		}
+
+		@Test
+		@DisplayName("should fail when variant is set but country is not on class level")
+		void shouldFailWhenVariantIsSetButCountryIsNotOnClassLevel() {
+			ExecutionEventRecorder eventRecorder = executeTestsForClass(ClassLevelInitializationFailureTestCase.class);
+
+			assertEquals(1, eventRecorder.getContainerFailedCount());
+		}
+	}
+
+	static class MethodLevelInitializationFailureTestCase {
+
+		@Test
+		@DefaultLocale(language = "de", variant = "ch")
+		void shouldFail() {
+		}
+	}
+
+	@DefaultLocale(language = "de", variant = "ch")
+	static class ClassLevelInitializationFailureTestCase {
+
+		@Test
+		void shouldFail() {
+		}
+	}
+
 }
