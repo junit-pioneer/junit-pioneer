0baf5279cf6d21fb8763e87613cbb4aa7b2c62c6
Daniel Kraus <daniel.kraus@mailbox.org>
2018-11-01 18:41:07 +0100

    Extract methods to store and reset system properties

--- src/main/java/org/junitpioneer/jupiter/SystemPropertyExtension.java
+++ src/main/java/org/junitpioneer/jupiter/SystemPropertyExtension.java
@@ -27,9 +27,7 @@ class SystemPropertyExtension implements BeforeEachCallback, AfterEachCallback {
 
 	@Override
 	public void beforeEach(final ExtensionContext context) throws Exception {
-		final Properties backup = new Properties();
-		backup.putAll(System.getProperties());
-		context.getStore(NAMESPACE).put(KEY, backup);
+		storeSystemProperties(context);
 
 		final Method testMethod = context.getTestMethod().get();
 		if (testMethod.isAnnotationPresent(ClearSystemProperty.class)) {
@@ -50,8 +48,18 @@ class SystemPropertyExtension implements BeforeEachCallback, AfterEachCallback {
 		}
 	}
 
+	private void storeSystemProperties(final ExtensionContext context) {
+		final Properties backup = new Properties();
+		backup.putAll(System.getProperties());
+		context.getStore(NAMESPACE).put(KEY, backup);
+	}
+
 	@Override
 	public void afterEach(final ExtensionContext context) throws Exception {
+		resetSystemProperties(context);
+	}
+
+	private void resetSystemProperties(final ExtensionContext context) {
 		final Properties backup = context.getStore(NAMESPACE).get(KEY, Properties.class);
 		System.setProperties(backup);
 	}
