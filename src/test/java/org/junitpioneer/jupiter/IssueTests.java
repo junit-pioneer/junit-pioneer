7add7cf807a2d20fc99b3f889638ab4f700d000a
bish <hardcore_army@web.de>
2019-12-12 18:59:11 +0100

    Writing Pseudocode for IssueTests-Class

--- src/test/java/org/junitpioneer/jupiter/IssueTests.java
+++ src/test/java/org/junitpioneer/jupiter/IssueTests.java
@@ -2,20 +2,31 @@ package org.junitpioneer.jupiter;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.engine.AbstractJupiterTestEngineTests;
 
 @ExtendWith(IssueExtension.class)
-public class IssueTests {
+public class IssueTests extends AbstractJupiterTestEngineTests {
+
 
 //    @Test
 //    void checkMethodNotAnnotated() {
-//        assertThat(IssueTestCase#testNoAnnotation).isNotAnnotatedWith(Issue.class);
-//        assertThat(IssueTestCase#testIsAnnotated).isAnnotatedWith(Issue.class);
+//         ExtensionContext context = JunitJupiterTestExecutionerMagicThing.getContext();
+//        executeTestsForClass(IssueTestCase.class);
+//
+//        if("testNoAnnotation".equals(exectuedMethod.getName())) {
+//            assertThat(exectuedMethod).isNotAnnotatedWith(Issue.class);
+//        }
 //    }
 //
 //    @Test
 //    void checkMethodIstAnnotated() {
-//        assertThat(IssueTestCase#testIsAnnotated).isAnnotatedWith(Issue.class);
-//        assertThat(IssueTestCase#testIsAnnotated).hasAnnotationValue(Issue.class, "Req 11");
+//        ExtensionContext context = JunitJupiterTestExecutionerMagicThing.getContext();
+//        executeTestsForClass(IssueTestCase.class);
+//
+//        if("testNoAnnotation".equals(exectuedMethod.getName())) {
+//            assertThat(exectuedMethod.isAnnotatedWith(Issue.class);
+//            assertThat(exectuedMethod.hasAnnotationValue(Issue.class, "Req 11");
+//        }
 //    }
 
     static class IssueTestCase {
