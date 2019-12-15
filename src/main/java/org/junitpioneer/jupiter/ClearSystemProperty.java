8abda8415755507367d131fe2092793f5e98c62d
Daniel Kraus <daniel.kraus@mailbox.org>
2019-12-15 17:52:11 +0100

    Update docs

--- src/main/java/org/junitpioneer/jupiter/ClearSystemProperty.java
+++ src/main/java/org/junitpioneer/jupiter/ClearSystemProperty.java
@@ -21,14 +21,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 /**
  * {@code @ClearSystemProperty} is a JUnit Jupiter extension to clear the value
  * of a system property for a test execution.
- *
- * <p> The key of the system property to be cleared must be specified via
- * {@link #key()}. After the annotated element has been executed, all system
- * properties will be restored to their original value.
- *
- * <p> {@code ClearSystemProperty} is repeatable and can be used on the method
- * and on the class level. If a class is annotated, the configured property will
- * be cleared for all tests inside that class.
+ * 
+ * <p>The key of the system property to be cleared must be specified via
+ * {@link #key()}. After the annotated element has been executed, After the
+ * annotated method has been executed, the initial default value is restored.
+ * 
+ * <p>{@code ClearSystemProperty} is repeatable and can be used on the method and
+ * on the class level. If a class is annotated, the configured property will be
+ * cleared for all tests inside that class.
  *
  * @since 0.4
  */
--- src/main/java/org/junitpioneer/jupiter/SetSystemProperty.java
+++ src/main/java/org/junitpioneer/jupiter/SetSystemProperty.java
@@ -21,14 +21,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 /**
  * {@code @SetSystemProperty} is a JUnit Jupiter extension to set the value of a
  * system property for a test execution.
- *
- * <p> The key and value of the system property to be set must be specified via
- * {@link #key()} and {@link #value()}. After the annotated element has been
- * executed, all system properties will be restored to their original value.
- *
- * <p> {@code SetSystemProperty} is repeatable and can be used on the method and
- * on the class level. If a class is annotated, the configured property will be
- * set for all tests inside that class. Any method level configurations will
+ * 
+ * <p>The key and value of the system property to be set must be specified via
+ * {@link #key()} and {@link #value()}. After the annotated method has been
+ * executed, the initial default value is restored.
+ * 
+ * <p>{@code SetSystemProperty} is repeatable and can be used on the method and on
+ * the class level. If a class is annotated, the configured property will be set
+ * for all tests inside that class. Any method level configurations will
  * override the class level configurations.
  *
  * @since 0.4
