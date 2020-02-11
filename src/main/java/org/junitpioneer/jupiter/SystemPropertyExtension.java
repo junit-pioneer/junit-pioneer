bb3a72425dfa8624709e71c1f5e8b6c72bf0abb7
Nicolai Parlog <nipa@codefx.org>
2020-02-11 21:52:53 +0100

    Remove and prevent star imports

--- src/main/java/org/junitpioneer/jupiter/SystemPropertyExtension.java
+++ src/main/java/org/junitpioneer/jupiter/SystemPropertyExtension.java
@@ -10,10 +10,16 @@
 
 package org.junitpioneer.jupiter;
 
-import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.toMap;
 
 import java.lang.annotation.Annotation;
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.extension.AfterAllCallback;
