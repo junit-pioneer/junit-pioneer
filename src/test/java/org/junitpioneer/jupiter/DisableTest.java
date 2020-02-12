b8d0bf4289bd277eb76921a55386f93a9d13e289
Mihaly Verhas <mihaly_verhas@epam.com>
2020-02-12 14:06:29 +0100

    Implemented test cases for DisableTest

--- src/test/java/org/junitpioneer/jupiter/DisableTest.java
+++ src/test/java/org/junitpioneer/jupiter/DisableTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -11,9 +12,17 @@ import org.junit.platform.engine.test.event.ExecutionEventRecorder;
 import org.junitpioneer.AbstractPioneerTestEngineTests;
 
 public class DisableTest extends AbstractPioneerTestEngineTests {
+    @Test
+    @DisplayName("Incorrectly configured, will throw an exception.")
+    void testIncorrect() {
+        ExecutionEventRecorder recorder = executeTests(DisableTestCases.class, "failedDisable(int,java.lang.String)");
+
+        assertThat(recorder.getTestFailedCount()).isEqualTo(9);
+    }
 
     @Test
-    void executesEightTimes_abortsOnce() {
+    @DisplayName("Disables tests for a matching input.")
+    void testSingle() {
         ExecutionEventRecorder recorder = executeTests(DisableTestCases.class, "disableSingle(int,java.lang.String)");
 
         assertThat(recorder.getTestAbortedCount()).isEqualTo(1);
@@ -21,15 +30,26 @@ public class DisableTest extends AbstractPioneerTestEngineTests {
     }
 
     @Test
-    void executesSixTimes_abortsThreeTimes() {
-        ExecutionEventRecorder recorder = executeTests(DisableTestCases.class, "disableContains(int,java.lang.String)");
+    @DisplayName("Providing a reason does not change the outcome.")
+    void testSingleWithReasoning() {
+        ExecutionEventRecorder recorder = executeTests(DisableTestCases.class, "disableSingleWithReason(int,java.lang.String)");
 
-        assertThat(recorder.getTestAbortedCount()).isEqualTo(3);
-        assertThat(recorder.getTestSuccessfulCount()).isEqualTo(6);
+        assertThat(recorder.getTestAbortedCount()).isEqualTo(1);
+        assertThat(recorder.getTestSuccessfulCount()).isEqualTo(8);
     }
 
     @Test
-    void executesFourTimes_abortsFiveTimes() {
+    @DisplayName("Can disable tests if a single param matches.")
+    void testContains() {
+        ExecutionEventRecorder recorder = executeTests(DisableTestCases.class, "disableContains(java.lang.String,java.lang.String)");
+
+        assertThat(recorder.getTestAbortedCount()).isEqualTo(5);
+        assertThat(recorder.getTestSuccessfulCount()).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("Given multiple conditions, will disable tests for any of them.")
+    void testCompound() {
         ExecutionEventRecorder recorder = executeTests(DisableTestCases.class, "disableCompound(int,java.lang.String)");
 
         assertThat(recorder.getTestAbortedCount()).isEqualTo(5);
@@ -39,7 +59,25 @@ public class DisableTest extends AbstractPioneerTestEngineTests {
     static class DisableTestCases {
         @ParameterizedTest
         @CsvSource({"1, a", "1, b", "1, c", "2, a", "2, b", "2, c", "3, a", "3, b", "3, c"})
+        public void failedDisable(int number, String text) {
+            Disable
+                .when(number, text)
+                .is(1, "a", "c");
+            assertThat(Arrays.asList(number, text)).isNotEqualTo(Arrays.asList(1, "a"));
+        }
+
+        @ParameterizedTest
+        @CsvSource({"1, a", "1, b", "1, c", "2, a", "2, b", "2, c", "3, a", "3, b", "3, c"})
         public void disableSingle(int number, String text) {
+            Disable
+                .when(number, text)
+                .is(1, "a");
+            assertThat(Arrays.asList(number, text)).isNotEqualTo(Arrays.asList(1, "a"));
+        }
+
+        @ParameterizedTest
+        @CsvSource({"1, a", "1, b", "1, c", "2, a", "2, b", "2, c", "3, a", "3, b", "3, c"})
+        public void disableSingleWithReason(int number, String text) {
             Disable
                 .when(number, text)
                 .is(1, "a")
@@ -48,13 +86,14 @@ public class DisableTest extends AbstractPioneerTestEngineTests {
         }
 
         @ParameterizedTest
-        @CsvSource({"1, a", "1, b", "1, c", "2, a", "2, b", "2, c", "3, a", "3, b", "3, c"})
-        public void disableContains(int number, String text) {
+        @CsvSource({"a, a", "a, b", "a, c", "b, a", "b, b", "b, c", "c, a", "c, b", "c, c"})
+        public void disableContains(String key, String value) {
             Disable
-                .when(number, text)
-                .contains(1)
-                .becauseOf("1 is the loneliest number.");
-            assertThat(number).isNotEqualTo(1);
+                .when(key, value)
+                .contains("a")
+                .becauseOf("should not matter if key or value");
+            assertThat(key).isNotEqualTo("a");
+            assertThat(value).isNotEqualTo("a");
         }
 
         @ParameterizedTest
