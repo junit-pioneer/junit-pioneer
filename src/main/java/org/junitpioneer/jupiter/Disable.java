bcf7e1d67369a687a74afa615c1c1ccea033ac8f
Mihaly Verhas <mihaly_verhas@epam.com>
2020-02-12 14:21:20 +0100

    Added copyright notice, applied formatting options.

--- src/main/java/org/junitpioneer/jupiter/Disable.java
+++ src/main/java/org/junitpioneer/jupiter/Disable.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2015-2020 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
 package org.junitpioneer.jupiter;
 
 import java.util.Arrays;
@@ -5,45 +15,44 @@ import java.util.Arrays;
 import org.opentest4j.TestAbortedException;
 
 public final class Disable {
-    private String reason;
-    private Object[] params;
+	private String reason;
+	private Object[] params;
 
-    private Disable(Object[] params) {
-        this.params = params;
-    }
+	private Disable(Object[] params) {
+		this.params = params;
+	}
 
-    public static Disable when(Object... params) {
-        return new Disable(params);
-    }
+	public static Disable when(Object... params) {
+		return new Disable(params);
+	}
 
-    public Disable contains(Object value) {
-        if (Arrays.asList(params).contains(value)) {
-            abortTest(value);
-        }
-        return this;
-    }
+	public Disable contains(Object value) {
+		if (Arrays.asList(params).contains(value)) {
+			abortTest(value);
+		}
+		return this;
+	}
 
-    public Disable is(Object... values) {
-        if (values.length != params.length) {
-            throw new IllegalArgumentException("Incorrect number of arguments for disabling tests, expected: "
-                + params.length
-                + ", but got: "
-                + values.length);
-        }
-        if (Arrays.equals(params, values)) {
-            abortTest(values);
-        }
-        return this;
-    }
+	public Disable is(Object... values) {
+		if (values.length != params.length) {
+			throw new IllegalArgumentException("Incorrect number of arguments for disabling tests, expected: "
+					+ params.length + ", but got: " + values.length);
+		}
+		if (Arrays.equals(params, values)) {
+			abortTest(values);
+		}
+		return this;
+	}
 
-    public void becauseOf(String reason) {
-        this.reason = reason;
-    }
+	public void becauseOf(String reason) {
+		this.reason = reason;
+	}
 
-    private void abortTest(Object... values) {
-        if (reason != null) {
-            throw new TestAbortedException("Test aborted for arguments " + Arrays.toString(values) + ", see: " + reason);
-        }
-        throw new TestAbortedException("Test aborted for arguments " + Arrays.toString(values));
-    }
+	private void abortTest(Object... values) {
+		if (reason != null) {
+			throw new TestAbortedException(
+				"Test aborted for arguments " + Arrays.toString(values) + ", see: " + reason);
+		}
+		throw new TestAbortedException("Test aborted for arguments " + Arrays.toString(values));
+	}
 }
--- src/test/java/org/junitpioneer/jupiter/DisableTest.java
+++ src/test/java/org/junitpioneer/jupiter/DisableTest.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2015-2020 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
 package org.junitpioneer.jupiter;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,102 +22,89 @@ import org.junit.platform.engine.test.event.ExecutionEventRecorder;
 import org.junitpioneer.AbstractPioneerTestEngineTests;
 
 public class DisableTest extends AbstractPioneerTestEngineTests {
-    @Test
-    @DisplayName("Incorrectly configured, will throw an exception.")
-    void testIncorrect() {
-        ExecutionEventRecorder recorder = executeTests(DisableTestCases.class, "failedDisable(int,java.lang.String)");
+	@Test
+	@DisplayName("Incorrectly configured, will throw an exception.")
+	void testIncorrect() {
+		ExecutionEventRecorder recorder = executeTests(DisableTestCases.class, "failedDisable(int,java.lang.String)");
 
-        assertThat(recorder.getTestFailedCount()).isEqualTo(9);
-    }
+		assertThat(recorder.getTestFailedCount()).isEqualTo(9);
+	}
 
-    @Test
-    @DisplayName("Disables tests for a matching input.")
-    void testSingle() {
-        ExecutionEventRecorder recorder = executeTests(DisableTestCases.class, "disableSingle(int,java.lang.String)");
+	@Test
+	@DisplayName("Disables tests for a matching input.")
+	void testSingle() {
+		ExecutionEventRecorder recorder = executeTests(DisableTestCases.class, "disableSingle(int,java.lang.String)");
 
-        assertThat(recorder.getTestAbortedCount()).isEqualTo(1);
-        assertThat(recorder.getTestSuccessfulCount()).isEqualTo(8);
-    }
+		assertThat(recorder.getTestAbortedCount()).isEqualTo(1);
+		assertThat(recorder.getTestSuccessfulCount()).isEqualTo(8);
+	}
 
-    @Test
-    @DisplayName("Providing a reason does not change the outcome.")
-    void testSingleWithReasoning() {
-        ExecutionEventRecorder recorder = executeTests(DisableTestCases.class, "disableSingleWithReason(int,java.lang.String)");
+	@Test
+	@DisplayName("Providing a reason does not change the outcome.")
+	void testSingleWithReasoning() {
+		ExecutionEventRecorder recorder = executeTests(DisableTestCases.class,
+			"disableSingleWithReason(int,java.lang.String)");
 
-        assertThat(recorder.getTestAbortedCount()).isEqualTo(1);
-        assertThat(recorder.getTestSuccessfulCount()).isEqualTo(8);
-    }
+		assertThat(recorder.getTestAbortedCount()).isEqualTo(1);
+		assertThat(recorder.getTestSuccessfulCount()).isEqualTo(8);
+	}
 
-    @Test
-    @DisplayName("Can disable tests if a single param matches.")
-    void testContains() {
-        ExecutionEventRecorder recorder = executeTests(DisableTestCases.class, "disableContains(java.lang.String,java.lang.String)");
+	@Test
+	@DisplayName("Can disable tests if a single param matches.")
+	void testContains() {
+		ExecutionEventRecorder recorder = executeTests(DisableTestCases.class,
+			"disableContains(java.lang.String,java.lang.String)");
 
-        assertThat(recorder.getTestAbortedCount()).isEqualTo(5);
-        assertThat(recorder.getTestSuccessfulCount()).isEqualTo(4);
-    }
+		assertThat(recorder.getTestAbortedCount()).isEqualTo(5);
+		assertThat(recorder.getTestSuccessfulCount()).isEqualTo(4);
+	}
 
-    @Test
-    @DisplayName("Given multiple conditions, will disable tests for any of them.")
-    void testCompound() {
-        ExecutionEventRecorder recorder = executeTests(DisableTestCases.class, "disableCompound(int,java.lang.String)");
+	@Test
+	@DisplayName("Given multiple conditions, will disable tests for any of them.")
+	void testCompound() {
+		ExecutionEventRecorder recorder = executeTests(DisableTestCases.class, "disableCompound(int,java.lang.String)");
 
-        assertThat(recorder.getTestAbortedCount()).isEqualTo(5);
-        assertThat(recorder.getTestSuccessfulCount()).isEqualTo(4);
-    }
+		assertThat(recorder.getTestAbortedCount()).isEqualTo(5);
+		assertThat(recorder.getTestSuccessfulCount()).isEqualTo(4);
+	}
 
-    static class DisableTestCases {
-        @ParameterizedTest
-        @CsvSource({"1, a", "1, b", "1, c", "2, a", "2, b", "2, c", "3, a", "3, b", "3, c"})
-        public void failedDisable(int number, String text) {
-            Disable
-                .when(number, text)
-                .is(1, "a", "c");
-            assertThat(Arrays.asList(number, text)).isNotEqualTo(Arrays.asList(1, "a"));
-        }
+	static class DisableTestCases {
+		@ParameterizedTest
+		@CsvSource({ "1, a", "1, b", "1, c", "2, a", "2, b", "2, c", "3, a", "3, b", "3, c" })
+		public void failedDisable(int number, String text) {
+			Disable.when(number, text).is(1, "a", "c");
+			assertThat(Arrays.asList(number, text)).isNotEqualTo(Arrays.asList(1, "a"));
+		}
 
-        @ParameterizedTest
-        @CsvSource({"1, a", "1, b", "1, c", "2, a", "2, b", "2, c", "3, a", "3, b", "3, c"})
-        public void disableSingle(int number, String text) {
-            Disable
-                .when(number, text)
-                .is(1, "a");
-            assertThat(Arrays.asList(number, text)).isNotEqualTo(Arrays.asList(1, "a"));
-        }
+		@ParameterizedTest
+		@CsvSource({ "1, a", "1, b", "1, c", "2, a", "2, b", "2, c", "3, a", "3, b", "3, c" })
+		public void disableSingle(int number, String text) {
+			Disable.when(number, text).is(1, "a");
+			assertThat(Arrays.asList(number, text)).isNotEqualTo(Arrays.asList(1, "a"));
+		}
 
-        @ParameterizedTest
-        @CsvSource({"1, a", "1, b", "1, c", "2, a", "2, b", "2, c", "3, a", "3, b", "3, c"})
-        public void disableSingleWithReason(int number, String text) {
-            Disable
-                .when(number, text)
-                .is(1, "a")
-                .becauseOf("single issue");
-            assertThat(Arrays.asList(number, text)).isNotEqualTo(Arrays.asList(1, "a"));
-        }
+		@ParameterizedTest
+		@CsvSource({ "1, a", "1, b", "1, c", "2, a", "2, b", "2, c", "3, a", "3, b", "3, c" })
+		public void disableSingleWithReason(int number, String text) {
+			Disable.when(number, text).is(1, "a").becauseOf("single issue");
+			assertThat(Arrays.asList(number, text)).isNotEqualTo(Arrays.asList(1, "a"));
+		}
 
-        @ParameterizedTest
-        @CsvSource({"a, a", "a, b", "a, c", "b, a", "b, b", "b, c", "c, a", "c, b", "c, c"})
-        public void disableContains(String key, String value) {
-            Disable
-                .when(key, value)
-                .contains("a")
-                .becauseOf("should not matter if key or value");
-            assertThat(key).isNotEqualTo("a");
-            assertThat(value).isNotEqualTo("a");
-        }
+		@ParameterizedTest
+		@CsvSource({ "a, a", "a, b", "a, c", "b, a", "b, b", "b, c", "c, a", "c, b", "c, c" })
+		public void disableContains(String key, String value) {
+			Disable.when(key, value).contains("a").becauseOf("should not matter if key or value");
+			assertThat(key).isNotEqualTo("a");
+			assertThat(value).isNotEqualTo("a");
+		}
 
-        @ParameterizedTest
-        @CsvSource({"1, a", "1, b", "1, c", "2, a", "2, b", "2, c", "3, a", "3, b", "3, c"})
-        public void disableCompound(int number, String text) {
-            Disable
-                .when(number, text)
-                .contains(1)
-                .is(3, "a")
-                .is(2, "b")
-                .becauseOf("something that should be fixed");
-            assertThat(number).isNotEqualTo(1);
-            assertThat(Arrays.asList(number, text)).isNotEqualTo(Arrays.asList(2, "b"));
-            assertThat(Arrays.asList(number, text)).isNotEqualTo(Arrays.asList(3, "a"));
-        }
-    }
+		@ParameterizedTest
+		@CsvSource({ "1, a", "1, b", "1, c", "2, a", "2, b", "2, c", "3, a", "3, b", "3, c" })
+		public void disableCompound(int number, String text) {
+			Disable.when(number, text).contains(1).is(3, "a").is(2, "b").becauseOf("something that should be fixed");
+			assertThat(number).isNotEqualTo(1);
+			assertThat(Arrays.asList(number, text)).isNotEqualTo(Arrays.asList(2, "b"));
+			assertThat(Arrays.asList(number, text)).isNotEqualTo(Arrays.asList(3, "a"));
+		}
+	}
 }
