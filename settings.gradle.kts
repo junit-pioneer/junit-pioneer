ba21e5256206fa5573ac54d8b472bc3cd9703370
Simon Schrottner <s.schrottner@netconomy.net>
2020-03-27 19:48:11 +0100

    Updating Gradle to 6.3 and building against JDK 14
    With this pull request we are updating to the latest
    gradle version. So we can run our builds against
    Java 14
    
    fixes #197
    fixes #196

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "junit-pioneer"
+
+plugins {
+  id("com.gradle.enterprise") version "3.2"
+}
+
+gradleEnterprise { 
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
