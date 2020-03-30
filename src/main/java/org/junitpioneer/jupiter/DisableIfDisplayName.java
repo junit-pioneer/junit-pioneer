1852e358bd5d1b33940b8dfa82e71dc107d2ce7a
nishant.vashisth <nishant.vashisth@deposit-solutions.com>
2020-03-30 20:10:31 +0200

    Check Style

--- src/main/java/org/junitpioneer/jupiter/DisableIfDisplayName.java
+++ src/main/java/org/junitpioneer/jupiter/DisableIfDisplayName.java
@@ -10,7 +10,10 @@
 
 package org.junitpioneer.jupiter;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.condition.DisabledIf;
