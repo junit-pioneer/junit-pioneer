3a655e751b47cb2f446ef94e49519ed7e7fa354d
Hancho2009 <mark-roesler@gmx.de>
2020-04-25 10:33:10 +0200

    Fix Javadoc of EnvironmentVariableExtension
    Corrects several documentation errors of the
    `EnvironmentVariableExtension`. The documentation of the
    `SystemPropertyExtension` and the `EnvironmentVariableExtension` were
    accidentally mixed up.
    
    Fixes: #237
    PR: #240
    
    [ci skip-release]

--- build.gradle.kts
+++ build.gradle.kts
@@ -104,7 +104,7 @@ tasks {
             includeTestsMatching("*Tests")
         }
         systemProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager")
-        // `SystemPropertyExtension` uses reflection to change environment variables;
+        // `EnvironmentVariableExtension` uses reflection to change environment variables;
         // this prevents the corresponding warning (and keeps working on Java 8)
         // IF YOU ADD MORE OPTIONS; CONSIDER REPLACING `-XX:+IgnoreUnrecognizedVMOptions WITH A CONDITIONAL
         jvmArgs(
