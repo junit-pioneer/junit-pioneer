417d1537c022db767cc2ca07331cf85b1c395664
Matthias Bünger <hardcore_army@web.de>
2020-05-02 11:40:30 +0200

    Change automatic module name to match other names

--- build.gradle.kts
+++ build.gradle.kts
@@ -137,7 +137,7 @@ tasks {
     jar {
         manifest {
             attributes(
-                    "Automatic-Module-Name" to "org.junitpioneer"
+                    "Automatic-Module-Name" to "org.junit-pioneer"
             )
         }
     }
