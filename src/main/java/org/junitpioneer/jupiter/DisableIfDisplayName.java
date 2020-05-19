a6498193aecdc93109b1233e30c02e6d2583ff1f
Nicolai Parlog <nipa@codefx.org>
2020-05-19 22:54:34 +0200

    Improve documentation

--- src/main/java/org/junitpioneer/jupiter/DisableIfDisplayName.java
+++ src/main/java/org/junitpioneer/jupiter/DisableIfDisplayName.java
@@ -24,18 +24,15 @@ import org.junit.jupiter.params.ParameterizedTest;
 
 /**
  * {@code @DisableIfDisplayName} is a JUnit Jupiter extension which can be used to
- * selectively disable {@link ParameterizedTest} based on their {@link ExtensionContext#getDisplayName()}.
+ * selectively disable {@link ParameterizedTest} based on their
+ * {@link ExtensionContext#getDisplayName() display name}.
  *
  * <p>
  * The extension is an {@link ExecutionCondition}, which validates dynamically registered tests.
  * This is highly useful since current {@link Disabled} or {@link DisabledIf} annotations disable
  * the whole test, but not the Parameterized tests selectively.
- *
- * If it is required to disable selective tests out of the plethora of dynamically
- * registered Parameterized tests, then we can utilize the following.
- *
- * Each repeatable annotation will be processed for each test and the test will be skipped if
- * any of them evaluates true against the display name.
+ * With {@code DisableIfDisplayName}, it is possible to selectively disable tests out of the
+ * plethora of dynamically registered parameterized tests.
  *
  * @since 0.5.6
  * @see DisableIfNameExtension
@@ -49,13 +46,15 @@ public @interface DisableIfDisplayName {
 	 *
 	 * Display names of the test cases to be disabled. The whole test case name can be passed as well as a sub string.
 	 * The values will be evaluated with {@link String#contains(CharSequence)} by default.
-	 * If, {@code isRegEx} is provided, the string will be evaluated with {@link String#matches(String)} against the display name
-	 * @return Test Case display name
+	 * If {@link #isRegEx} is {@code true}, the string will be evaluated with {@link String#matches(String)}
+	 * against the display name
+	 *
+	 * @return test case display name
 	 */
 	String[] value();
 
 	/**
-	 * @return if the {@code value} is to be evaluated as regular expression or sub-string
+	 * @return whether the {@code value} is to be evaluated as regular expression or substring
 	 */
 	boolean isRegEx() default false;
 
