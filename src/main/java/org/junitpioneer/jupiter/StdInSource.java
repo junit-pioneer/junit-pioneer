170e2c2a4c316fe1179493bbdcc466ac385f15f6
Mihály Verhás <misi.verhas@gmail.com>
2020-05-28 09:35:07 +0200

    Provide StdIOExtension
    The `StdIOExtension` adds a simple way to test classes that
    read from the standard input (`System.in`)
    or write to the standard output (`System.out`).
    
    Input text is providing via annotations and passed through
    a simple Pioneer own `StdIn` implementation.
    
    Printed text is redirected to and captured by a simple Pioneer
    own `StdOut` implementation. 
    
    
    Closes: #34
    PR: #227

---
+++ src/main/java/org/junitpioneer/jupiter/StdInSource.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2015-2020 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junitpioneer.jupiter;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Provide values that the {@link StdIOExtension extension} will read instead of reading the
+ * standard input ({@code System.in}).
+ * This should be used with {@link org.junitpioneer.jupiter.StdIOExtension.StdIn}.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface StdInSource {
+
+	/**
+	 * Provides the intercepted standard input with values.
+	 */
+	String[] value();
+
+}
