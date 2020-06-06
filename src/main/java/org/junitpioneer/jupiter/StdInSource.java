fc93e399f6a4d6667fdba594cd73f8c2859be7c7
Matthias Bünger <hardcore_army@web.de>
2020-06-06 10:46:14 +0200

    Executing StdIoExecution singlethreaded

--- src/main/java/org/junitpioneer/jupiter/StdInSource.java
+++ src/main/java/org/junitpioneer/jupiter/StdInSource.java
@@ -10,16 +10,23 @@
 
 package org.junitpioneer.jupiter;
 
+import static org.junit.jupiter.api.parallel.ExecutionMode.SAME_THREAD;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.junit.jupiter.api.parallel.Execution;
+
 /**
  * Provide values that the {@link StdIOExtension extension} will read instead of reading the
  * standard input ({@code System.in}).
  * This should be used with {@link org.junitpioneer.jupiter.StdIOExtension.StdIn}.
+ *
+ * <p>The extension will run single threaded to guarantee thread safety!</p>
  */
+@Execution(SAME_THREAD)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface StdInSource {
