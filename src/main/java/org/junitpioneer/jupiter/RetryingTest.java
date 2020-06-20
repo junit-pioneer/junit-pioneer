0ca9f4ca5abaaf41e578da07a6635a89d7ae2450
Matthias Bünger <Bukama@users.noreply.github.com>
2020-06-20 12:32:48 +0200

    Rename `RepeatFailedTest` to `RetryingTest` (#233 / #236)
    #233 reported a problem that was eventually tracked down to
    `@RepeatFailedTest` and `@Test` being both applied to the same
    method. While the extension's documentation clearly advises against
    that, we think the name contributed to the confusion:
    `@RepeatFailedTest` may sound too much like needing to be applied to
    a method that is already a test, i.e. already annotated with `@Test`.
    We hope that `@RetryingTest` prevents that.
    
    Closes: #233 
    PR: #236

--- src/main/java/org/junitpioneer/jupiter/RetryingTest.java
+++ src/main/java/org/junitpioneer/jupiter/RetryingTest.java
@@ -23,11 +23,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.parallel.Execution;
 
 /**
- * {@code @RepeatFailedTest} is a JUnit Jupiter extension that repeats
+ * {@code @RetryingTest} is a JUnit Jupiter extension that retries
  * a failing test a certain number of times before the test actually
  * shows up as failing.
  *
- * If annotated with {@code @RepeatFailedTest(n)}, a test method is
+ * If annotated with {@code @RetryingTest(n)}, a test method is
  * executed as long as it keeps failing, but no more than {@code n}
  * times. That means all actual executions - except possibly the
  * last - have failed. In contrast, all executions - except possibly
@@ -37,16 +37,17 @@ import org.junit.jupiter.api.parallel.Execution;
  * Each ignored/aborted or failed execution includes the underlying
  * exception.
  *
- * <p>{@code @RepeatFailedTest} has a number of limitations:</p>
+ * <p>{@code @RetryingTest} has a number of limitations:</p>
  *
  * <ul>
  *     <li>it can only be applied to methods</li>
+ *     <li>methods annotated with this annotation <b>MUST NOT</b> be annotated with {@code @Test}
+ *         to avoid multiple executions!</li>
  *     <li>it can't be used with other {@link TestTemplate}-based mechanisms
  *         like {@code org.junit.jupiter.api.RepeatedTest @RepeatedTest} or
  *         {@code org.junit.jupiter.params.ParameterizedTest @ParameterizedTest}</li>
- *     <li>it can't be used with
- *         {@code org.junit.jupiter.api.DynamicTest @DynamicTest}</li>
- *     <li>all repetitions are run sequentially, even when used with
+ *     <li>it can't be used with {@code org.junit.jupiter.api.DynamicTest @DynamicTest}</li>
+ *     <li>all retries are run sequentially, even when used with
  *         <a href="https://junit.org/junit5/docs/current/user-guide/#writing-tests-parallel-execution">parallel test execution</a></li>
  * </ul>
  *
@@ -58,12 +59,12 @@ import org.junit.jupiter.api.parallel.Execution;
  */
 @Target({ METHOD, ANNOTATION_TYPE })
 @Retention(RetentionPolicy.RUNTIME)
-// the extension is not thread-safe, so it forces execution of all repetitions
+// the extension is not thread-safe, so it forces execution of all retries
 // onto the same thread
 @Execution(SAME_THREAD)
-@ExtendWith(RepeatFailedTestExtension.class)
+@ExtendWith(RetryingTestExtension.class)
 @TestTemplate
-public @interface RepeatFailedTest {
+public @interface RetryingTest {
 
 	/**
 	 * Specifies how often the test is executed at most.
