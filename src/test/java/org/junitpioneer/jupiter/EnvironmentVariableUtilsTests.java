bc4d50bcf972c042a9dced4c25f624dde3fbf06b
Hancho2009 <mark-roesler@gmx.de>
2020-07-14 22:13:34 +0200

    Add tests with enabled SecurityManager to env extension (#241 / #242)
    The documentation mentions that without proper permissions an enabled
    security manager will not give access to the internals we need to
    change environment variables. These tests confirm that.
    
    Closes: #241
    PR: #242

--- src/test/java/org/junitpioneer/jupiter/EnvironmentVariableUtilsTests.java
+++ src/test/java/org/junitpioneer/jupiter/EnvironmentVariableUtilsTests.java
@@ -10,12 +10,17 @@
 
 package org.junitpioneer.jupiter;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.security.AccessControlException;
+import java.security.Policy;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("JUnitPioneer system environment utilities")
@@ -25,11 +30,56 @@ class EnvironmentVariableUtilsTests {
 	void theEnvironmentIsNotCorruptedAfterSet() {
 		EnvironmentVariableUtils.set("A_VARIABLE", "A value");
 
-		// By using this method, the entire environment is read and copied from the field
-		// ProcessEnvironment.theEnvironment. If that field is corrupted by a String having been stored
-		// as key or value, this copy operation will fail with a ClassCastException.
+		/* By using this method, the entire environment is read and copied from the field
+		   ProcessEnvironment.theEnvironment. If that field is corrupted by a String having been stored
+		   as key or value, this copy operation will fail with a ClassCastException. */
 		Map<String, String> environmentCopy = new HashMap<>(System.getenv());
-		assertEquals("A value", environmentCopy.get("A_VARIABLE"));
+		assertThat(environmentCopy.get("A_VARIABLE")).isEqualTo("A value");
+	}
+
+	/*
+	 * The documentation mentions that without proper permissions an enabled security manager will not
+	 * give access to the internals we need to change environment variables. These tests confirm that.
+	 */
+	@ClearEnvironmentVariable(key = "TEST")
+	@Nested
+	class With_SecurityManager {
+
+		@Test
+		@SetSystemProperty(key = "java.security.policy", value = "file:src/test/resources/default-testing.policy")
+		void shouldThrowAccessControlExceptionWithDefaultSecurityManager() {
+			//@formatter:off
+			executeWithSecurityManager(
+					() -> assertThatThrownBy(() -> EnvironmentVariableUtils.set("TEST", "TEST"))
+							.isInstanceOf(AccessControlException.class));
+			//@formatter:on
+		}
+
+		@Test
+		@SetSystemProperty(key = "java.security.policy", value = "file:src/test/resources/reflect-permission-testing.policy")
+		void shouldModifyEnvironmentVariableIfPermissionIsGiven() {
+			executeWithSecurityManager(() -> {
+				assertThatCode(() -> EnvironmentVariableUtils.set("TEST", "TEST")).doesNotThrowAnyException();
+				assertThat(System.getenv("TEST")).isEqualTo("TEST");
+			});
+		}
+
+		/*
+		 * This needs to be done during the execution of the test method and cannot be moved to setup/tear down
+		 * because junit uses reflection and the default SecurityManager prevents that.
+		 */
+		private void executeWithSecurityManager(Runnable runnable) {
+			Policy.getPolicy().refresh();
+			final SecurityManager original = System.getSecurityManager();
+			System.setSecurityManager(new SecurityManager());
+			try {
+				runnable.run();
+			}
+			finally {
+				System.setSecurityManager(original);
+			}
+		}
+
 	}
 
 }
