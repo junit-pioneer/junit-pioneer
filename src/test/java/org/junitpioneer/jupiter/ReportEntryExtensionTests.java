dccf86090cda246d3876550dd57d5f5be483403e
Mihaly Verhas <misi.verhas@gmail.com>
2020-07-15 10:51:19 +0200

    Remove unused import
    Signed-off-by: Mihaly Verhas <misi.verhas@gmail.com>

--- src/test/java/org/junitpioneer/jupiter/ReportEntryExtensionTests.java
+++ src/test/java/org/junitpioneer/jupiter/ReportEntryExtensionTests.java
@@ -23,7 +23,6 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtensionConfigurationException;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
