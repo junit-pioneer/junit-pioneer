ebeca587b69038a7f5916d19f9ee7086ed8fae79
Matthias Bünger <hardcore_army@web.de>
2020-07-31 19:16:22 +0200

    Merge master into current

--- src/test/java/org/junitpioneer/jupiter/StdIoExtensionTests.java
+++ src/test/java/org/junitpioneer/jupiter/StdIoExtensionTests.java
@@ -210,7 +210,7 @@ public class StdIoExtensionTests {
 		@DisplayName("without input but StdIn parameter, an exception is thrown")
 		void withoutInputWithStdInParameter() {
 			ExecutionResults results = executeTestMethodWithParameterTypes(IllegalConfigurations.class,
-				"noInputButStdIn", "org.junitpioneer.jupiter.StdIn");
+				"noInputButStdIn", StdIn.class);
 
 			assertThat(results).hasSingleFailedTest();
 		}
