0a18f552251af1c2831711873a7725d6430b0bae
Matthias Bünger <hardcore_army@web.de>
2020-10-03 09:56:55 +0200

    Merge

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,10 +38,10 @@ val junitVersion : String by project
 
 dependencies {
 	implementation(platform("org.junit:junit-bom:$junitVersion"))
-	implementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = "5.$junitMinorVersion")
-	implementation(group = "org.junit.jupiter", name = "junit-jupiter-params", version = "5.$junitMinorVersion")
-	implementation(group = "org.junit.platform", name = "junit-platform-commons", version = "1.$junitMinorVersion")
-    implementation(group = "org.junit.platform", name = "junit-platform-launcher", version = "1.$junitMinorVersion")
+	implementation(group = "org.junit.jupiter", name = "junit-jupiter-api")
+	implementation(group = "org.junit.jupiter", name = "junit-jupiter-params")
+	implementation(group = "org.junit.platform", name = "junit-platform-commons")
+    implementation(group = "org.junit.platform", name = "junit-platform-launcher")
     // For Java 8 provided would be enough as its part of the JRE,
     // but Java 11 and 14 need this dependency cause of Jigsaw
     implementation(group = "javax.xml.bind", name ="jaxb-api", version = "2.3.1")
