57544af67d4dd4b689dc2cc930a4e07e6cf545a1
Matthias Bünger <hardcore_army@web.de>
2020-10-19 14:14:59 +0200

    Cover toString with test

---
+++ src/test/java/org/junitpioneer/jupiter/issue/IssuedTestCaseTests.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2015-2020 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junitpioneer.jupiter.issue;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public final class IssuedTestCaseTests {
+
+	@Test
+	void testToString() {
+		String expected = "IssuedTestCase{uniqueName='myName', issueId='REQ-123', result='SUCCESSFUL'}";
+
+		IssuedTestCase sut = new IssuedTestCase("myName", "REQ-123", "SUCCESSFUL");
+
+		String result = sut.toString();
+
+		assertThat(result).isEqualTo(expected);
+	}
+
+}
