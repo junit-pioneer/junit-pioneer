50873798ca6e2825e01575de1aaa6b13a1ed7433
Matthias Bünger <hardcore_army@web.de>
2020-10-19 14:01:42 +0200

    Update module-info.java

--- src/main/module/module-info.java
+++ src/main/module/module-info.java
@@ -2,6 +2,7 @@ module org.junitpioneer {
 	requires org.junit.jupiter.api;
 	requires org.junit.jupiter.params;
 	requires org.junit.platform.commons;
+	requires org.junit.platform.launcher;
 
 	exports org.junitpioneer.vintage;
 	exports org.junitpioneer.jupiter;
