f9f46c8d6d6290c2bbc72f07e5e87e90a6a92a78
Mihaly Verhas <misi.verhas@gmail.com>
2020-11-02 18:18:03 +0100

    Add documentation based on DisableIfDisplayName
    Signed-off-by: Mihaly Verhas <misi.verhas@gmail.com>

--- src/main/java/org/junitpioneer/jupiter/params/DisableIfParameter.java
+++ src/main/java/org/junitpioneer/jupiter/params/DisableIfParameter.java
@@ -17,6 +17,25 @@ import java.lang.annotation.Target;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 
+/**
+ * {@code @DisableIfParameter} is a JUnit Jupiter extension that can
+ * be used to selectively disable a {@link org.junit.jupiter.params.ParameterizedTest}
+ * based on their parameter values as defined by {@link Object#toString()}.
+ *
+ * <p>The extension utilizes Jupiter's {@link org.junit.jupiter.api.extension.InvocationInterceptor}.
+ * It's important to note that since it's marked as {@link org.apiguardian.api.API.Status#EXPERIMENTAL}
+ * it might be removed without prior notice.
+ * Unlike {@link org.junit.jupiter.api.Disabled} annotations, this extension doesn't disable the whole test method.
+ * With {@code DisableIfParameter}, it is possible to selectively disable tests out of the plethora
+ * of dynamically registered parameterized tests.</p>
+ *
+ * <p>If neither {@link DisableIfParameter#contains() contains} nor
+ * {@link DisableIfParameter#matches() matches} is configured, the extension will throw an exception.
+ * It is possible to configure both, in which case the test gets disabled if at least one substring
+ * was found <em>or</em> at least one regular expression matched.</p>
+ *
+ * @see DisableIfParameterExtension
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @ExtendWith(DisableIfParameterExtension.class)
