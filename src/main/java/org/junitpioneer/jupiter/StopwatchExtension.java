925b2bc1c9dd5ed79abe1bed8335ee61cd3dfab7
Matthias Bünger <Bukama@users.noreply.github.com>
2020-12-07 19:33:57 +0100

    Stopwatch extension (#38 / #182) (#182)
    Implement the Stopwatch extension (#38 / #182)
    
    This PR moves the Stopwatch extension from JUnit Jupiter
    examples to JUnit Pioneer. The extension measures the
    elapsed time of each test (which is executed with the
    extension) and publishes the the result as a ReportEntry.
    
    closes: #38 
    PR: #182

---
+++ src/main/java/org/junitpioneer/jupiter/StopwatchExtension.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2015-2020 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junitpioneer.jupiter;
+
+import java.time.Clock;
+
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
+
+/**
+ * The StopwatchExtension implements callback methods for the {@code @Stopwatch} annotation.
+ */
+class StopwatchExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
+
+	private final Clock clock = Clock.systemUTC();
+	private static final Namespace NAMESPACE = Namespace.create(StopwatchExtension.class);
+	static final String STORE_KEY = "StopwatchExtension";
+
+	@Override
+	public void beforeTestExecution(ExtensionContext context) {
+		storeNowAsLaunchTime(context);
+	}
+
+	@Override
+	public void afterTestExecution(ExtensionContext context) {
+		calculateAndReportElapsedTime(context);
+	}
+
+	/**
+	 * Stores the current time for the method in the execution context.
+	 *
+	 * @param context Extension context of the class
+	 */
+	void storeNowAsLaunchTime(ExtensionContext context) {
+		context.getStore(NAMESPACE).put(context.getUniqueId(), clock.instant().toEpochMilli());
+	}
+
+	/**
+	 * Loads the stored time for method from the execution context.
+	 *
+	 * @param context Extension context of the class
+	 */
+	long loadLaunchTime(ExtensionContext context) {
+		return context.getStore(NAMESPACE).get(context.getUniqueId(), long.class);
+	}
+
+	/**
+	 * Calculates the elapsed time method and publishes it to the execution context.
+	 *
+	 * @param context Extension context of the class
+	 */
+	void calculateAndReportElapsedTime(ExtensionContext context) {
+		long launchTime = loadLaunchTime(context);
+		long elapsedTime = clock.instant().toEpochMilli() - launchTime;
+
+		String message = String.format("Execution of '%s' took [%d] ms.", context.getDisplayName(), elapsedTime);
+		context.publishReportEntry(STORE_KEY, message);
+	}
+
+}
---
+++ src/test/java/org/junitpioneer/jupiter/StopwatchExtensionTests.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2015-2020 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junitpioneer.jupiter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junitpioneer.testkit.ExecutionResults;
+import org.junitpioneer.testkit.PioneerTestKit;
+import org.junitpioneer.testkit.assertion.PioneerAssert;
+
+@DisplayName("Stopwatch extension ")
+public class StopwatchExtensionTests {
+
+	@DisplayName("should be executed with annotation on class level and report an entry for test method")
+	@Test
+	void runClassLevelAnnotationTest() {
+
+		ExecutionResults results = PioneerTestKit
+				.executeTestClass(StopwatchExtensionTests.ClassLevelAnnotationTest.class);
+
+		PioneerAssert.assertThat(results).hasNumberOfReportEntries(1);
+
+		String methodName = "stopwatchExtensionShouldBeExecutedWithAnnotationOnClassLevel";
+		assertStringStartWithUnitAndContainsName(results, methodName);
+
+	}
+
+	@DisplayName("should be executed with annotation on class level and test method and report an entry for test method")
+	@Test
+	void runClassAndMethodLevelAnnotationTest() {
+		String methodName = "stopwatchExtensionShouldBeExecutedWithAnnotationOnClassAndMethodLevel";
+
+		ExecutionResults results = PioneerTestKit
+				.executeTestClass(StopwatchExtensionTests.ClassAndMethodLevelAnnotationTest.class);
+		PioneerAssert.assertThat(results).hasNumberOfReportEntries(1);
+
+		assertStringStartWithUnitAndContainsName(results, methodName);
+	}
+
+	@DisplayName("should be executed with annotation on test method and report an entry for test method")
+	@Test
+	void runMethodLevelAnnotationTest() {
+		String methodName = "stopwatchExtensionShouldBeExecutedOnWithAnnotationOnMethodLevel";
+
+		ExecutionResults results = PioneerTestKit
+				.executeTestMethod(StopwatchExtensionTests.MethodLevelAnnotationTest.class, methodName);
+		PioneerAssert.assertThat(results).hasNumberOfReportEntries(1);
+
+		assertStringStartWithUnitAndContainsName(results, methodName);
+	}
+
+	@DisplayName("should not be executed and therefore no entry should be published")
+	@Test
+	void runAnnotationTest() {
+		String methodName = "stopwatchExtensionShouldNotBeExecuted";
+
+		ExecutionResults results = PioneerTestKit
+				.executeTestMethod(StopwatchExtensionTests.NonAnnotationTest.class, methodName);
+		PioneerAssert.assertThat(results).hasNumberOfReportEntries(0);
+
+	}
+
+	private void assertStringStartWithUnitAndContainsName(ExecutionResults results, String methodName) {
+		PioneerAssert.assertThat(results).hasNumberOfReportEntries(1).andThen(entry -> {
+			assertThat(entry.getKey()).isEqualTo(StopwatchExtension.STORE_KEY);
+			assertThat(entry.getValue())
+					.matches(String.format("Execution of '%s\\(\\)' took \\[[0-9]*\\] ms.", methodName));
+		});
+	}
+
+	/**
+	 * Inner test class for testing the class level annotation.
+	 */
+	@Stopwatch
+	static class ClassLevelAnnotationTest {
+
+		@Test
+		void stopwatchExtensionShouldBeExecutedWithAnnotationOnClassLevel() {
+		}
+
+	}
+
+	/**
+	 * Inner test class for testing the method level annotation.
+	 */
+	static class MethodLevelAnnotationTest {
+
+		@Stopwatch
+		@Test
+		void stopwatchExtensionShouldBeExecutedOnWithAnnotationOnMethodLevel() {
+		}
+
+	}
+
+	/**
+	 * Inner test class for testing the class level annotation.
+	 */
+	@Stopwatch
+	static class ClassAndMethodLevelAnnotationTest {
+
+		@Stopwatch
+		@Test
+		void stopwatchExtensionShouldBeExecutedWithAnnotationOnClassAndMethodLevel() {
+		}
+
+	}
+
+	/**
+	 * Inner test class for testing a not annotated method / class annotation.
+	 */
+	static class NonAnnotationTest {
+
+		@Test
+		void stopwatchExtensionShouldNotBeExecuted() {
+		}
+
+	}
+
+}
