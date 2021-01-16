33749e0d7c9364d3ada8cf1a72aea502bee9826a
Nicolai Parlog <nipa@codefx.org>
2021-01-16 09:55:39 +0100

    More clearly document/log env. var. reflective warning (#387 / #404)
    The environment variable extension uses reflection to change the
    otherwise immutable variables. On Java 9+, this leads to warnings or
    errors and this change makes it easier for users to understand the
    reason behind that:
    
    * Before this change, we printed a related warning via report entries,
      but those are often lost in the wild. We now log an additional
      warning to `System.out`. While we initially considered `System.err`
      it is rather invasive, particularly since, with good configuration,
      the module system won't emit a warning and hence it's only Pioneer
      polluting `System.err` - not good.
    * This change also expands the documentation by including the exact
      warning/error messages, how to prevent them, and links to build tool
      solutions.
    
    Closes: #387
    PR: #404

--- src/main/java/org/junitpioneer/jupiter/EnvironmentVariableExtension.java
+++ src/main/java/org/junitpioneer/jupiter/EnvironmentVariableExtension.java
@@ -52,8 +52,16 @@ class EnvironmentVariableExtension extends AbstractEntryBasedExtension<String, S
 	@Override
 	protected void reportWarning(ExtensionContext context) {
 		boolean wasReported = REPORTED_WARNING.getAndSet(true);
-		if (!wasReported)
-			context.publishReportEntry(WARNING_KEY, WARNING_VALUE);
+		if (wasReported)
+			return;
+
+		// Log as report entry and to System.out - check docs for reasons, but why System.out?
+		// Because report entries lack tool support and are easily lost and System.err is
+		// too invasive (particularly since, with good configuration, the module system won't
+		// print a warning and hence it's only Pioneer polluting System.err - not good).
+		// System.out seemed like a good compromise.
+		context.publishReportEntry(WARNING_KEY, WARNING_VALUE);
+		System.out.println(WARNING_KEY + ": " + WARNING_VALUE); //NOSONAR
 	}
 
 	@Override
