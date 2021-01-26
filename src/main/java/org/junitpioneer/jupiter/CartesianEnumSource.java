35a44f0ffa6bbfe2c3894ad613882a8d968fd468
Nicolai Parlog <nipa@codefx.org>
2021-01-26 20:02:44 +0100

    Oh, the humanity!

--- src/main/java/org/junitpioneer/jupiter/CartesianEnumSource.java
+++ src/main/java/org/junitpioneer/jupiter/CartesianEnumSource.java
@@ -20,7 +20,6 @@ import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.util.Objects;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
