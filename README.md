8a05cc3df524fc950c807774b118b48967f873f8
Stefano Cordio <scordio@users.noreply.github.com>
2021-04-06 21:24:04 +0200

    Add automatic type detection to `@CartesianEnumSource` (#414 / #453)
    `@CartesianEnumSource` is now able to auto-detect the enum type
    from the method signature.
    
    Closes: #414
    PR: #453

--- README.md
+++ README.md
@@ -113,7 +113,7 @@ The least we can do is to thank them and list some of their accomplishments here
 * [Cory Thomas](https://github.com/dump247) contributed the `minSuccess` attribute in [retrying tests](https://junit-pioneer.org/docs/retrying-test/) (#408 / #430)
 * [Daniel Kraus](https://github.com/beatngu13) fixed bugs in the environment variable and system property extensions (#432 / #433 and #448 / #449)
 * [Slawomir Jaranowski](https://github.com/slawekjaranowski) Migrate to new Shipkit plugins (#410 / #419)
-* [Stefano Cordio](https://github.com/scordio) contributed [the Cartesian Enum source](https://junit-pioneer.org/docs/cartesian-product/#cartesianenumsource) (#379 / #409)
+* [Stefano Cordio](https://github.com/scordio) contributed [the Cartesian Enum source](https://junit-pioneer.org/docs/cartesian-product/#cartesianenumsource) (#379 / #409 and #414 / #453)
 
 #### 2020
 
--- src/main/java/org/junitpioneer/jupiter/CartesianEnumArgumentsProvider.java
+++ src/main/java/org/junitpioneer/jupiter/CartesianEnumArgumentsProvider.java
@@ -13,29 +13,37 @@ package org.junitpioneer.jupiter;
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toSet;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.EnumSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.platform.commons.PreconditionViolationException;
+import org.junitpioneer.internal.PioneerAnnotationUtils;
 
 /**
- * This is basically a copy of Jupiter's {@code EnumArgumentsProvider},
+ * This is basically an enhanced copy of Jupiter's {@code EnumArgumentsProvider},
  * except it does NOT support {@code @ParameterizedTest}.
  */
 class CartesianEnumArgumentsProvider implements CartesianAnnotationConsumer<CartesianEnumSource>, ArgumentsProvider {
 
-	private EnumSet<?> constants;
+	private CartesianEnumSource enumSource;
 
 	@Override
-	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public void accept(CartesianEnumSource enumSource) {
-		Class enumClass = enumSource.value();
-		this.constants = EnumSet.allOf(enumClass);
+		this.enumSource = enumSource;
+	}
 
+	@Override
+	public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+		Set<? extends Enum<?>> constants = getEnumConstants(context);
 		CartesianEnumSource.Mode mode = enumSource.mode();
 		String[] declaredConstantNames = enumSource.names();
 		if (declaredConstantNames.length > 0) {
@@ -44,14 +52,50 @@ class CartesianEnumArgumentsProvider implements CartesianAnnotationConsumer<Cart
 			if (uniqueNames.size() != declaredConstantNames.length)
 				throw new PreconditionViolationException("Duplicate enum constant name(s) found in " + enumSource);
 
-			mode.validate(enumSource, uniqueNames);
-			this.constants.removeIf(constant -> !mode.select(constant, uniqueNames));
+			mode.validate(enumSource, constants, uniqueNames);
+			constants.removeIf(constant -> !mode.select(constant, uniqueNames));
 		}
+		return constants.stream().map(Arguments::of);
 	}
 
-	@Override
-	public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
-		return constants.stream().map(Arguments::of);
+	private <E extends Enum<E>> Set<? extends E> getEnumConstants(ExtensionContext context) {
+		Class<E> enumClass = determineEnumClass(context);
+		return EnumSet.allOf(enumClass);
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private <E extends Enum<E>> Class<E> determineEnumClass(ExtensionContext context) {
+		Class enumClass = enumSource.value();
+		if (enumClass.equals(NullEnum.class)) {
+			Method method = context.getRequiredTestMethod();
+			Class<?>[] parameterTypes = method.getParameterTypes();
+
+			if (parameterTypes.length <= 0)
+				throw new PreconditionViolationException(
+					"Test method must declare at least one parameter: " + method.toGenericString());
+
+			Class<?> parameterType = parameterTypes[determineParameterTypeIndex(method)];
+
+			if (!Enum.class.isAssignableFrom(parameterType))
+				throw new PreconditionViolationException(String
+						.format(
+							"Parameter of type %s must reference an Enum type (alternatively, use the annotation's 'value' attribute to specify the type explicitly): %s",
+							parameterType, method.toGenericString()));
+
+			enumClass = parameterType;
+		}
+		return enumClass;
+	}
+
+	private int determineParameterTypeIndex(Method method) {
+		List<Annotation> argumentSources = PioneerAnnotationUtils
+				.findAnnotatedAnnotations(method, ArgumentsSource.class);
+
+		return IntStream
+				.range(0, argumentSources.size())
+				.filter(i -> enumSource == argumentSources.get(i))
+				.findFirst()
+				.orElseThrow(() -> new PreconditionViolationException("CartesianEnumSource annotation not found"));
 	}
 
 }
