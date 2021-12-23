a4b29c515e985c90c2fed1f3e5ae6444b8d258b8
Matthias Bünger <Bukama@users.noreply.github.com>
2021-12-23 17:30:27 +0100

    Clean ...jupiter's package-info (#554 / 556)
    Updates `package-info.java` of the `org.junitpioneer.jupiter` package,
    removing old / non-public classes and adding not listed packages.
    
    Closes: #554
    PR: #556

--- src/main/java/org/junitpioneer/jupiter/package-info.java
+++ src/main/java/org/junitpioneer/jupiter/package-info.java
@@ -3,18 +3,23 @@
  *
  * <p>Check out the following types for details:
  * <ul>
- *     <li>{@link org.junitpioneer.jupiter.CartesianProductTest}</li>
- *     <li>{@link org.junitpioneer.jupiter.ClearSystemProperty} and {@link org.junitpioneer.jupiter.SetSystemProperty}</li>
  *     <li>{@link org.junitpioneer.jupiter.ClearEnvironmentVariable} and {@link org.junitpioneer.jupiter.SetEnvironmentVariable}</li>
+ *     <li>{@link org.junitpioneer.jupiter.ClearSystemProperty} and {@link org.junitpioneer.jupiter.SetSystemProperty}</li>
  *     <li>{@link org.junitpioneer.jupiter.DefaultLocale} and {@link org.junitpioneer.jupiter.DefaultTimeZone}</li>
  *     <li>{@link org.junitpioneer.jupiter.DisabledUntil}</li>
  *     <li>{@link org.junitpioneer.jupiter.ReportEntry}</li>
  *     <li>{@link org.junitpioneer.jupiter.RetryingTest}</li>
- *     <li>{@link org.junitpioneer.jupiter.StdIoExtension} and {@link org.junitpioneer.jupiter.StdIo}</li>
+ *     <li>{@link org.junitpioneer.jupiter.StdIo}</li>
  *     <li>{@link org.junitpioneer.jupiter.Stopwatch}</li>
  * </ul>
  *
- * Also check the {@link org.junitpioneer.jupiter.params params} package.
+ * Also check the following packages:
+ * <ul>
+ *     <li>{@link org.junitpioneer.jupiter.cartesian cartesian} (successor of {@link org.junitpioneer.jupiter.CartesianProductTest})</li>
+ *     <li>{@link org.junitpioneer.jupiter.issue issue}</li>
+ *     <li>{@link org.junitpioneer.jupiter.params params}</li>
+ * </ul>
+ *
  */
 
 package org.junitpioneer.jupiter;
