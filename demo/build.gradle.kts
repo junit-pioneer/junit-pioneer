735f13a53a6db807aac33522b68a549edaf7c6c9
Mihaly Verhas <misi.verhas@gmail.com>
2021-12-25 15:30:39 +0100

    Fix build

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -17,7 +17,23 @@ dependencies {
     implementation(group = "org.junit.platform", name = "junit-platform-commons")
     implementation(group = "org.junit.platform", name = "junit-platform-launcher")
 
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-engine")
+    testImplementation(group = "org.junit.platform", name = "junit-platform-testkit")
+
+    testImplementation(group = "org.assertj", name = "assertj-core", version = "3.20.2")
+    testImplementation(group = "org.mockito", name = "mockito-core", version = "3.12.4")
+    testImplementation(group = "com.google.jimfs", name = "jimfs", version = "1.2")
+
+    testRuntimeOnly(group = "org.apache.logging.log4j", name = "log4j-core", version = "2.14.1")
+    testRuntimeOnly(group = "org.apache.logging.log4j", name = "log4j-jul", version = "2.14.1")
+
     implementation(rootProject)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    withJavadocJar()
+    withSourcesJar()
 }
 
 spotless {
@@ -34,17 +50,15 @@ spotless {
 }
 
 tasks {
+    compileTestJava {
+        options.encoding = "UTF-8"
+    }
+
     test {
         useJUnitPlatform()
         filter {
             includeTestsMatching("*Tests")
         }
         systemProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager")
-        // `EnvironmentVariableExtension` uses reflection to change environment variables;
-        // this prevents the corresponding warning (and keeps working on Java 8)
-        // IF YOU ADD MORE OPTIONS; CONSIDER REPLACING `-XX:+IgnoreUnrecognizedVMOptions WITH A CONDITIONAL
-        jvmArgs(
-                "-XX:+IgnoreUnrecognizedVMOptions",
-                "--add-opens=java.base/java.util=ALL-UNNAMED")
     }
 }
