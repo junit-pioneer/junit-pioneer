cab6c9d6888b268c397a2c91a5ca043bed1c971b
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2022-01-07 18:37:25 +0000

    WIP - first draft for resource/package-info.java and amendment to docs/temporary-directory.adoc

--- src/main/java/org/junitpioneer/jupiter/resource/package-info.java
+++ src/main/java/org/junitpioneer/jupiter/resource/package-info.java
@@ -1,5 +1,18 @@
 /**
- * TODO
+ * This package contains various classes pertaining to "resources": anything that needs to be injected into any number
+ * of tests and which may need to be started up or torn down.
+ * <p>Check out the following types for details on resources in general:
+ * <ul>
+ *     <li>{@link org.junitpioneer.jupiter.resource.Resource}</li>
+ *     <li>{@link org.junitpioneer.jupiter.resource.ResourceFactory}</li>
+ *     <li>{@link org.junitpioneer.jupiter.resource.New}</li>
+ *     <li>{@link org.junitpioneer.jupiter.resource.Shared}</li>
+ * </ul>
+ * <p>Check out the following types for details on the "temporary directory" resource:
+ * <ul>
+ *     <li>{@link org.junitpioneer.jupiter.resource.TemporaryDirectory}</li>
+ *     <li>{@link org.junitpioneer.jupiter.resource.Dir}</li>
+ * </ul>
  */
 
 package org.junitpioneer.jupiter.resource;
