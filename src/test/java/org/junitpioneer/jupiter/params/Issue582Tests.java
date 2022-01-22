be60453d9b4c1360e859c06d024ef94dd7aac23b
Daniel Kraus <daniel.kraus@mailbox.org>
2022-01-22 18:19:50 +0100

    Spotless!

--- src/test/java/org/junitpioneer/jupiter/params/Issue582Tests.java
+++ src/test/java/org/junitpioneer/jupiter/params/Issue582Tests.java
@@ -1,13 +1,23 @@
+/*
+ * Copyright 2016-2021 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
 package org.junitpioneer.jupiter.params;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.junitpioneer.jupiter.ClearEnvironmentVariable;
 import org.junitpioneer.jupiter.SetEnvironmentVariable;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @ClearEnvironmentVariable(key = "some variable")
-public class Issue582Tests {
+class Issue582Tests {
 
 	@Test
 	@SetEnvironmentVariable(key = "some variable", value = "new value")
