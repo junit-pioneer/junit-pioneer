b1b7c354ca2017828fe476a7f962811d3762e7c4
Daniel Kraus <daniel.kraus@mailbox.org>
2022-01-22 18:12:54 +0100

    Add succeeding test

---
+++ src/test/java/org/junitpioneer/jupiter/params/Issue582Tests.java
@@ -0,0 +1,18 @@
+package org.junitpioneer.jupiter.params;
+
+import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.ClearEnvironmentVariable;
+import org.junitpioneer.jupiter.SetEnvironmentVariable;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ClearEnvironmentVariable(key = "some variable")
+public class Issue582Tests {
+
+	@Test
+	@SetEnvironmentVariable(key = "some variable", value = "new value")
+	void test() {
+		assertThat(System.getenv("some variable")).isEqualTo("new value");
+	}
+
+}
