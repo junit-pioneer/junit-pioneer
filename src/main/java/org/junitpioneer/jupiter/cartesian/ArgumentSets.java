33905c1e3b1b176c33aa3ba82bc516a87fcdae33
Nicolai Parlog <nipa@codefx.org>
2022-02-17 21:27:42 +0100

    Of course...

--- src/main/java/org/junitpioneer/jupiter/cartesian/ArgumentSets.java
+++ src/main/java/org/junitpioneer/jupiter/cartesian/ArgumentSets.java
@@ -1,12 +1,22 @@
+/*
+ * Copyright 2016-2021 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
 package org.junitpioneer.jupiter.cartesian;
+
+import static java.util.stream.Collectors.toList;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
 
 /**
  * Class for defining sets to a {@code CartesianTest} execution with arguments for each parameter
