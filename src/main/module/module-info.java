f461bd1252f992ce4de6229b443576bcf9eb6a0a
Daniel Kraus <daniel.kraus@mailbox.org>
2022-02-20 20:36:37 +0100

    Also open vintage to org.junit.platform.commons

--- src/main/module/module-info.java
+++ src/main/module/module-info.java
@@ -9,6 +9,7 @@ module org.junitpioneer {
 	exports org.junitpioneer.jupiter.cartesian;
 	exports org.junitpioneer.jupiter.params;
 
+	opens org.junitpioneer.vintage to org.junit.platform.commons;
 	opens org.junitpioneer.jupiter to org.junit.platform.commons;
 	opens org.junitpioneer.jupiter.cartesian to org.junit.platform.commons;
 	opens org.junitpioneer.jupiter.params to org.junit.platform.commons;
