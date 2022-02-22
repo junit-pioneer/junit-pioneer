79d925b3ebaab475c7cb8ef471df31adaf9d792c
Mihaly Verhas <misi.verhas@gmail.com>
2022-02-22 10:33:35 +0100

    Make RangeClass package-private

--- src/main/java/org/junitpioneer/jupiter/params/RangeClass.java
+++ src/main/java/org/junitpioneer/jupiter/params/RangeClass.java
@@ -24,7 +24,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.ANNOTATION_TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface RangeClass {
+@interface RangeClass {
 
 	Class<? extends Range> value();
 
