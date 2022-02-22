b66fb27bb0684d44282798e3d1434c0950dcdec0
Mihaly Verhas <misi.verhas@gmail.com>
2022-02-22 10:43:29 +0100

    Remove unused import

--- src/main/java/org/junitpioneer/jupiter/params/RangeClass.java
+++ src/main/java/org/junitpioneer/jupiter/params/RangeClass.java
@@ -10,7 +10,6 @@
 
 package org.junitpioneer.jupiter.params;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
