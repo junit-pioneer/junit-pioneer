39713896c4299af4a8b2abbe0725ed43f1cb6a5b
Nicolai Parlog <nipa@codefx.org>
2022-03-10 21:53:50 +0100

    Rename test case class

--- src/test/java/org/junitpioneer/jupiter/issue/IssueExtensionIntegrationTests.java
+++ src/test/java/org/junitpioneer/jupiter/issue/IssueExtensionIntegrationTests.java
@@ -39,7 +39,7 @@ public class IssueExtensionIntegrationTests {
 				.create()
 				.execute(LauncherDiscoveryRequestBuilder
 						.request()
-						.selectors(DiscoverySelectors.selectClass(IssueIntegrationCases.class))
+						.selectors(DiscoverySelectors.selectClass(IssueIntegrationTestCase.class))
 						.build());
 
 		List<IssueTestSuite> issueTestSuites = StoringIssueProcessor.ISSUE_TEST_SUITES;
@@ -62,7 +62,7 @@ public class IssueExtensionIntegrationTests {
 			assertThat(testCase.result()).isEqualTo(Status.FAILED);
 	}
 
-	static class IssueIntegrationCases {
+	static class IssueIntegrationTestCase {
 
 		@Test
 		@Issue("Poem #1")
