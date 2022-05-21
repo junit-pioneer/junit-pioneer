26b88bfffb5ac1fd593d515af6a4849e7d54c8e8
Mihaly Verhas <misi.verhas@gmail.com>
2022-05-21 15:54:24 +0200

    Spotless

--- src/test/java/org/junitpioneer/jupiter/json/NoJsonParserConfiguredExceptionTest.java
+++ src/test/java/org/junitpioneer/jupiter/json/NoJsonParserConfiguredExceptionTest.java
@@ -1,8 +1,18 @@
+/*
+ * Copyright 2016-2022 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
 package org.junitpioneer.jupiter.json;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
 
 class NoJsonParserConfiguredExceptionTest {
 
@@ -10,4 +20,5 @@ class NoJsonParserConfiguredExceptionTest {
 	void shouldBeUnchecked() {
 		assertThat(new NoJsonParserConfiguredException()).isInstanceOf(RuntimeException.class);
 	}
+
 }
