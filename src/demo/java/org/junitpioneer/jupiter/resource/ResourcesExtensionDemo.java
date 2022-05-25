6e86104fae931cb7f196f9fb4737011651fc7a59
Nicolai Parlog <nipa@codefx.org>
2022-05-25 10:55:28 +0200

    Format demo code

--- src/demo/java/org/junitpioneer/jupiter/resource/ResourcesExtensionDemo.java
+++ src/demo/java/org/junitpioneer/jupiter/resource/ResourcesExtensionDemo.java
@@ -10,28 +10,25 @@
 
 package org.junitpioneer.jupiter.resource;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class ResourcesExtensionDemo {
 
-    // tag::create_new_resource_demo[]
-    void test(@New(TemporaryDirectory.class) Path tempDir) {
-        // Test code goes here, e.g.,
-        assertTrue(Files.exists(tempDir));
-    }
-    // end::create_new_resource_demo[]
+	// tag::create_new_resource_demo[]
+	void test(@New(TemporaryDirectory.class) Path tempDir) {
+		// Test code goes here, e.g.,
+		assertTrue(Files.exists(tempDir));
+	}
+	// end::create_new_resource_demo[]
 
-    // tag::create_new_resource_with_arg_demo[]
-    void testWithArg(
-            @New(value = TemporaryDirectory.class, arguments = { "customDirectoryName" })
-            Path tempDir
-    ) {
-        // Test code goes here, e.g.,
-        assertTrue(tempDir.endsWith("customDirectoryName"));
-    }
-    // end::create_new_resource_with_arg_demo[]
+	// tag::create_new_resource_with_arg_demo[]
+	void testWithArg(@New(value = TemporaryDirectory.class, arguments = { "customDirectoryName" }) Path tempDir) {
+		// Test code goes here, e.g.,
+		assertTrue(tempDir.endsWith("customDirectoryName"));
+	}
+	// end::create_new_resource_with_arg_demo[]
 
 }
