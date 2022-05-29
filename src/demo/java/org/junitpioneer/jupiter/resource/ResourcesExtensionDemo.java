1f54bde9c025862b34b36adb0a6705e2764026a2
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2022-05-29 21:12:53 +0100

    WIP - document how to create @Shared resources

--- src/demo/java/org/junitpioneer/jupiter/resource/ResourcesExtensionDemo.java
+++ src/demo/java/org/junitpioneer/jupiter/resource/ResourcesExtensionDemo.java
@@ -17,12 +17,17 @@ import java.nio.file.Path;
 
 public class ResourcesExtensionDemo {
 
-	// tag::create_new_resource_demo[]
-	void test(@New(TemporaryDirectory.class) Path tempDir) {
+	// tag::create_new_resources_demo[]
+	void test1(@New(TemporaryDirectory.class) Path tempDir) {
 		// Test code goes here, e.g.,
 		assertTrue(Files.exists(tempDir));
 	}
-	// end::create_new_resource_demo[]
+
+	void test2(@New(TemporaryDirectory.class) Path tempDir) {
+		// Test code goes here, e.g.,
+		assertTrue(Files.exists(tempDir));
+	}
+	// end::create_new_resources_demo[]
 
 	// tag::create_new_resource_with_arg_demo[]
 	void testWithArg(@New(value = TemporaryDirectory.class, arguments = { "customDirectoryName" }) Path tempDir) {
@@ -31,4 +36,50 @@ public class ResourcesExtensionDemo {
 	}
 	// end::create_new_resource_with_arg_demo[]
 
+	// tag::create_shared_resource_demo[]
+	void sharedResourceTest1(@Shared(factory = TemporaryDirectory.class, name = "sharedTempDir") Path sharedTempDir) {
+		// Test code goes here, e.g.,
+		assertTrue(Files.exists(sharedTempDir));
+	}
+
+	void sharedResourceTest2(@Shared(factory = TemporaryDirectory.class, name = "sharedTempDir") Path sharedTempDir) {
+		// Test code goes here, e.g.,
+		assertTrue(Files.exists(sharedTempDir));
+	}
+	// end::create_shared_resource_demo[]
+
+	// tag::create_multiple_shared_resources_demo[]
+	void firstSharedResource1(@Shared(factory = TemporaryDirectory.class, name = "first") Path first) {
+		// Test code working with first shared resource...
+	}
+
+	void firstSharedResource2(@Shared(factory = TemporaryDirectory.class, name = "first") Path first) {
+		// Test code working with first shared resource...
+	}
+
+	void secondSharedResource(@Shared(factory = TemporaryDirectory.class, name = "second") Path second) {
+		// Test code working with second shared resource...
+	}
+	// end::create_multiple_shared_resources_demo[]
+
 }
+
+// tag::create_global_shared_resource_demo_first[]
+class FirstTest {
+
+	void test(@Shared(factory = TemporaryDirectory.class, name = "globalTempDir", scope = Scope.GLOBAL) Path tempDir) {
+		// Test code using the global shared resource...
+	}
+
+}
+// end::create_global_shared_resource_demo_first[]
+
+// tag::create_global_shared_resource_demo_second[]
+class SecondTest {
+
+	void test(@Shared(factory = TemporaryDirectory.class, name = "globalTempDir", scope = Scope.GLOBAL) Path tempDir) {
+		// Test code using the global shared resource...
+	}
+
+}
+// end::create_global_shared_resource_demo_second[]
