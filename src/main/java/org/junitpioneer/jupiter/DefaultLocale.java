2fdb10014be7db85e975b6cdd39769815ee601bb
Matthias Bünger <hardcore_army@web.de>
2022-08-31 17:25:57 +0200

    Spotless

--- src/main/java/org/junitpioneer/jupiter/DefaultLocale.java
+++ src/main/java/org/junitpioneer/jupiter/DefaultLocale.java
@@ -10,13 +10,13 @@
 
 package org.junitpioneer.jupiter;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import org.junit.jupiter.api.extension.ExtendWith;
 
 /**
  * {@code @DefaultLocale} is a JUnit Jupiter extension to change the value
