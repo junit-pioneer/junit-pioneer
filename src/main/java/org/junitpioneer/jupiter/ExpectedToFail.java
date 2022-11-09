973170106433a7099d6312cd076c9833958a4f34
Nicolai Parlog <nipa@codefx.org>
2022-11-09 13:11:17 +0100

    Further polish documentation

--- src/main/java/org/junitpioneer/jupiter/ExpectedToFail.java
+++ src/main/java/org/junitpioneer/jupiter/ExpectedToFail.java
@@ -22,16 +22,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 /**
  * {@code @ExpectedToFail} is a JUnit Jupiter extension to mark test methods as temporarily
- * 'expected to fail'.
- * Such test methods will still be executed but when they result in a test failure or error
- * the test will be aborted.
- * However, if the test method unexpectedly executes successfully, it is marked as failure
- * to let the developer know that the test is now successful and that the {@code @ExpectedToFail}
- * annotation can be removed.
+ * 'expected to fail'. Such test methods will still be executed but when they result in a test
+ * failure or error the test will be aborted. However, if the test method unexpectedly executes
+ * successfully, it is marked as failure to let the developer know that the test is now
+ * successful and that the {@code @ExpectedToFail} annotation can be removed.
  *
  * <p>The big difference compared to JUnit's {@link org.junit.jupiter.api.Disabled @Disabled}
  * annotation is that the developer is informed as soon as a test is successful again.
- * This helps avoiding writing duplicate tests by accident and counteracts the accumulation
+ * This helps to avoid creating duplicate tests by accident and counteracts the accumulation
  * of disabled tests over time.
  *
  * <p>The annotation can only be used on methods and as meta-annotation on other annotation types.
@@ -39,8 +37,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
  * as {@link org.junit.jupiter.api.Test @Test}. Otherwise the annotation has no effect.
  *
  * <p><b>Important:</b> This annotation is <b>not</b> intended as a way to mark test methods
- * which intentionally cause exceptions.
- * Such test methods should use {@link org.junit.jupiter.api.Assertions#assertThrows(Class, org.junit.jupiter.api.function.Executable) assertThrows}
+ * which intentionally cause exceptions. Such test methods should use
+ * {@link org.junit.jupiter.api.Assertions#assertThrows(Class, org.junit.jupiter.api.function.Executable) assertThrows}
  * or similar means to explicitly test for a specific exception class being thrown by a
  * specific action.
  *
@@ -48,17 +46,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
  * <a href="https://junit-pioneer.org/docs/expected-to-fail-tests/" target="_top">the documentation on <code>@ExpectedToFail</code></a>.
  * </p>
  *
+ * @since 1.8.0
  * @see org.junit.jupiter.api.Disabled
  */
+@Documented
+@Retention(RUNTIME)
 /*
- * Implementation note:
  * Only supports METHOD and ANNOTATION_TYPE as targets but not test classes because there
  * it is not clear what the 'correct' behavior would be when only a few test methods
  * execute successfully. Would the developer then have to remove the @ExpectedToFail annotation
  * from the test class and annotate methods individually?
  */
-@Documented
-@Retention(RUNTIME)
 @Target({ METHOD, ANNOTATION_TYPE })
 @ExtendWith(ExpectedToFailExtension.class)
 public @interface ExpectedToFail {
