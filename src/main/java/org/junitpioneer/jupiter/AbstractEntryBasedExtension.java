3880e6ea94d6126846064f9d1ac27071fde4cdf7
Eric <24287468+eeverman@users.noreply.github.com>
2023-02-16 14:07:29 -0600

    Update src/main/java/org/junitpioneer/jupiter/AbstractEntryBasedExtension.java
    Format improvement
    
    Co-authored-by: Daniel Kraus <daniel.kraus@mailbox.org>

--- src/main/java/org/junitpioneer/jupiter/AbstractEntryBasedExtension.java
+++ src/main/java/org/junitpioneer/jupiter/AbstractEntryBasedExtension.java
@@ -179,7 +179,7 @@ abstract class AbstractEntryBasedExtension<K, V, C extends Annotation, S extends
 	}
 
 	/**
-	 * Restore the complete original state of the entries as they were prior to this ExtensionContext,
+	 * Restore the complete original state of the entries as they were prior to this {@code ExtensionContext},
 	 * if the complete state was initially stored in a BeforeXXX event.
 	 *
 	 * @param context The ExtensionContext which may have a bulk backup stored
