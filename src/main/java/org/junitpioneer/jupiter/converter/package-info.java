af7ef62cecf6eceb6fe736e63e4d69bb13c503c9
Mihaly Verhas <misi.verhas@gmail.com>
2023-07-09 21:13:05 +0200

    Working out bits and bobs

---
+++ src/main/java/org/junitpioneer/jupiter/converter/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * This package contains argument converters.
+ *
+ * <p>See the following types for details.</p>
+ *
+ * <ul>
+ *     <li>{@link org.junitpioneer.jupiter.converter.ByteArrayConversion}</li>
+ * </ul>
+ *
+ * @see org.junit.jupiter.params.converter.ArgumentConverter
+ * @see org.junit.jupiter.params.converter.TypedArgumentConverter
+ */
+
+package org.junitpioneer.jupiter.converter;
--- src/test/java/org/junitpioneer/jupiter/converter/ByteArrayConversionTests.java
+++ src/test/java/org/junitpioneer/jupiter/converter/ByteArrayConversionTests.java
@@ -14,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.junitpioneer.jupiter.converter.ByteArrayConversion.ByteOrder;
 
 public class ByteArrayConversionTests {
 
