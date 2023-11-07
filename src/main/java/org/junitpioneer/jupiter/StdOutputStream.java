020c70aeff2d55a13e35fed94021a287a6cb2050
Nicolai Parlog <nicolai@nipafx.dev>
2023-11-07 13:31:42 +0100

    Add documentation for `capturedLines()`

--- src/main/java/org/junitpioneer/jupiter/StdOutputStream.java
+++ src/main/java/org/junitpioneer/jupiter/StdOutputStream.java
@@ -41,6 +41,17 @@ abstract class StdOutputStream extends OutputStream {
 	}
 
 	/**
+	 * The {@link StdOutputStream#capturedString()}, divided on the line separator.
+	 *
+	 * <p>This includes leading, inner, and trailing empty lines but does not include the potential empty string
+	 * that comes after a trailing line separator. (The exact algorithm is based on but behaves differently
+	 * from {@link String#split(String)}.)</p>
+	 *
+	 * <p>Because the return value does not include a trailing empty line that comes from a trailing line separator,
+	 * it can't be used to distinguish the cases where the last line was created by a {@code print} or a
+	 * {@code println}. For more details and examples on this, see
+	 * <a href="https://junit-pioneer.org/docs/standard-input-output/" target="_top">the documentation on standard input/output</a>.</p>
+	 *
 	 * @return the lines that were written to {@code System.out} or {@code System.err}
 	 */
 	public String[] capturedLines() {
