38a20c7294c5be529410dc55d59cd58a27dd5fce
Mihaly Verhas <misi.verhas@gmail.com>
2023-11-30 20:31:03 +0100

    Update tests

--- src/test/java/org/junitpioneer/jupiter/FlakyExtensionTests.java
+++ src/test/java/org/junitpioneer/jupiter/FlakyExtensionTests.java
@@ -10,13 +10,90 @@
 
 package org.junitpioneer.jupiter;
 
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+import static org.junitpioneer.testkit.PioneerTestKit.executeTestMethod;
+import static org.junitpioneer.testkit.assertion.PioneerAssert.assertThat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtensionConfigurationException;
+import org.junitpioneer.testkit.ExecutionResults;
 
 public class FlakyExtensionTests {
 
 	@Test
-	@Flaky(value = 3)
-	void test() {
+	@DisplayName("completely disregards Flaky annotation if initial Test is successful")
+	void disregarded() {
+		ExecutionResults results = executeTestMethod(FlakyTestCases.class, "negativeValuePassing");
+
+		assertThat(results).hasNumberOfSucceededContainers(2);
+		assertThat(results).hasSingleSucceededTest();
+	}
+
+	@Test
+	@DisplayName("throws an ExtensionConfigurationException for negative value if initial Test is failing")
+	void invalidValue() {
+		ExecutionResults results = executeTestMethod(FlakyTestCases.class, "negativeValueFailing");
+
+		assertThat(results).hasNumberOfAbortedTests(1);
+		assertThat(results).hasSingleFailedContainer().withExceptionInstanceOf(ExtensionConfigurationException.class);
+	}
+
+	@Test
+	@DisplayName("failing test annotated with @Flaky and value 3 should run three times, abort twice and fail once")
+	void alwaysFailing() {
+		ExecutionResults results = executeTestMethod(FlakyTestCases.class, "alwaysFail");
+
+		assertThat(results).hasNumberOfAbortedTests(2);
+		assertThat(results)
+				.hasSingleFailedTest()
+				.withExceptionInstanceOf(IllegalStateException.class)
+				.hasMessage("Always failing");
+	}
+
+	@Test
+	@DisplayName("once failing test annotated with @Flaky and value 3 should run three times, abort once then succeed")
+	void failsOnlyFirstTime() {
+		ExecutionResults results = executeTestMethod(FlakyTestCases.class, "failsOnlyOnFirstInvocation");
+
+		assertThat(results).hasSingleSucceededTest();
+		assertThat(results).hasSingleSkippedTest();
+		assertThat(results).hasSingleAbortedTest();
+	}
+
+	@TestInstance(PER_CLASS)
+	static class FlakyTestCases {
+
+		private int executionCount;
+
+		@Test
+		@Flaky(value = -1)
+		void negativeValuePassing() {
+		}
+
+		@Test
+		@Flaky(-1)
+		void negativeValueFailing() {
+			Assertions.fail();
+		}
+
+		@Test
+		@Flaky(3)
+		void alwaysFail() {
+			throw new IllegalStateException("Always failing");
+		}
+
+		@Test
+		@Flaky(3)
+		void failsOnlyOnFirstInvocation() {
+			executionCount++;
+			if (executionCount == 1) {
+				throw new IllegalArgumentException();
+			}
+		}
+
 	}
 
 }
