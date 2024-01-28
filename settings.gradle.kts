35b7487a1f16852a3791ddeb15482058a08bfae9
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-01-28 22:36:14 +0000

    Hide warning:
    > Configure project :
    The 'sonarqube' task depends on compile tasks. This behavior is now deprecated and will be removed in version 5.x. To avoid implicit compilation, set property 'sonar.gradle.skipCompile' to 'true' and make sure your project is compiled, before analysis has started.
    The 'sonar' task depends on compile tasks. This behavior is now deprecated and will be removed in version 5.x. To avoid implicit compilation, set property 'sonar.gradle.skipCompile' to 'true' and make sure your project is compiled, before analysis has started.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,10 @@ gradleEnterprise {
 		termsOfServiceAgree = "yes"
 	}
 }
+
+// See https://community.sonarsource.com/t/sonar-gradle-skipcompile-is-not-working/102710/9
+// TODO move this to a better place once https://sonarsource.atlassian.net/browse/SONARGRADL-134 is resolved.
+// > The 'sonar' / 'sonarqube' task depends on compile tasks.
+// > This behavior is now deprecated and will be removed in version 5.x.
+// > To avoid implicit compilation, set property 'sonar.gradle.skipCompile' to 'true' and make sure your project is compiled, before analysis has started.
+System.setProperty("sonar.gradle.skipCompile", "true")
