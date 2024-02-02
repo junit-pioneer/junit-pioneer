8d52776e3c8495462933c33888e31834a70d1eb2
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-02 09:04:53 +0000

    Remove link

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,6 @@ gradleEnterprise {
 	}
 }
 
-// See https://community.sonarsource.com/t/sonar-gradle-skipcompile-is-not-working/102710/9
 // TODO move this to a better place once https://sonarsource.atlassian.net/browse/SONARGRADL-134 is resolved.
 // > The 'sonar' / 'sonarqube' task depends on compile tasks.
 // > This behavior is now deprecated and will be removed in version 5.x.
