78de1e8daf2a4663abdf8accdf312029eba84d5b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-07 18:53:10 +0000

    Fix Deprecation
    Using a toolchain installed via auto-provisioning, but having no toolchain repositories configured. This behavior is deprecated. Consider defining toolchain download repositories, otherwise the build might fail in cle
    an environments; see https://docs.gradle.org/8.5/userguide/toolchains.html#sub:download_repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "junit-pioneer"
 
 plugins {
 	id("com.gradle.enterprise") version "3.16.2"
+	id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
 }
 
 gradleEnterprise { 
