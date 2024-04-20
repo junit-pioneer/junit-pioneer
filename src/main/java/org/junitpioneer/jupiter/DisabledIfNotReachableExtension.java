9956490b0cc5bfc1066d933d1f69e42c15330466
Matthias Bünger <hardcore_army@web.de>
2024-04-20 15:17:14 +0200

    Savecommit

--- src/main/java/org/junitpioneer/jupiter/DisabledIfNotReachableExtension.java
+++ src/main/java/org/junitpioneer/jupiter/DisabledIfNotReachableExtension.java
@@ -10,15 +10,15 @@
 
 package org.junitpioneer.jupiter;
 
-import static java.lang.String.format;
-import static org.junit.jupiter.api.extension.ConditionEvaluationResult.disabled;
-import static org.junit.jupiter.api.extension.ConditionEvaluationResult.enabled;
-import static org.junitpioneer.internal.PioneerAnnotationUtils.findClosestEnclosingAnnotation;
-
 import org.junit.jupiter.api.extension.ConditionEvaluationResult;
 import org.junit.jupiter.api.extension.ExecutionCondition;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
+
+import static java.lang.String.format;
+import static org.junit.jupiter.api.extension.ConditionEvaluationResult.disabled;
+import static org.junit.jupiter.api.extension.ConditionEvaluationResult.enabled;
+import static org.junitpioneer.internal.PioneerAnnotationUtils.findClosestEnclosingAnnotation;
 
 class DisabledIfNotReachableExtension implements ExecutionCondition {
 
@@ -36,41 +36,49 @@ class DisabledIfNotReachableExtension implements ExecutionCondition {
 
 		var config = readConfigurationFromAnnotation(optAnnotation.get());
 
-		boolean reachable = pingUrl(config.getUrl(), config.getTimeOut());
-
-		if (reachable) {
-			return enabled(format("%s is enabled because %s is reachable", context.getUniqueId(), config.getUrl()));
-		} else {
-			return disabled(format("%s is disabled because %s could not be reached in %dms", context.getUniqueId(),
-				config.getUrl(), config.getTimeOut()));
-		}
-
 	}
 
 	/**
 	 * Pings an HTTP URL. This effectively sends a HEAD request and returns
 	 * {@code true} if the response code is in the 200-399 range.
 	 *
-	 * (From <a href="https://stackoverflow.com/users/157882/balusc">BalusC</a> on StackOverflow:
-	 * <a href="https://stackoverflow.com/a/3584332/2525313">Preferred Java way to ping an HTTP URL for availability</a>)
+	 * Based on <a href="https://stackoverflow.com/users/157882/balusc">BalusC</a>'s answer on StackOverflow to
+	 * <a href="https://stackoverflow.com/a/3584332/2525313">Preferred Java way to ping an HTTP URL for availability</a>
+	 * but with JDK 11 HTTP-Client.
 	 *
-	 * @param url
-	 * 		The HTTP URL to be pinged.
-	 * @param timeoutMillis
-	 * 		The timeout in millis for both the connection timeout and the
-	 * 		response read timeout. Note that the total timeout is effectively
-	 * 		two times the given timeout.
+	 * @param config
+	 *  Configuration, including the url and a timeout value, based on annotation value
+	 * @param context
+	 * 	Extension context to get the unique ID of the test to be executed
+	 *
 	 *
 	 * @return {@code true} if the given HTTP URL has returned response
 	 * code 200-399 on a HEAD request within the given timeout, otherwise
 	 * {@code false}.
 	 */
-	private static boolean pingUrl(String url, int timeoutMillis) {
+	private static ConditionEvaluationResult pingUrl(DisabledIfNotReachableConfiguration config, ExtensionContext context) {
+
+		String url = config.getUrl();
+		int timeout = config.getTimeOut();
 
 		https: //openjdk.org/groups/net/httpclient/intro.html
 
-		return false;
+		boolean reachable = false;
 
+		if (reachable) {
+			return enabled(format("%s is enabled because %s is reachable", context.getUniqueId(), config.getUrl()));
+		} else {
+			return disabled(format("%s is disabled because %s could not be reached in %dms", context.getUniqueId(),
+					config.getUrl(), config.getTimeOut()));
+		}
+
+
+//	 * @param config
+//				* 		The HTTP URL to be pinged.
+//				* @param timeoutMillis
+//				* 		The timeout in millis for both the connection timeout and the
+//	 * 		response read timeout. Note that the total timeout is effectively
+//				* 		two times the given timeout.
 		//		// Otherwise an exception may be thrown on invalid SSL certificates
 		//		String httpUrl = url.replaceFirst("^https", "http");
 		//		try {
