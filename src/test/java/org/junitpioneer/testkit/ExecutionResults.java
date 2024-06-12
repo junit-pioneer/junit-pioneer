1227a5f416c4c0160fcdaeac099fb75f4c6169ed
boris-faniuk-n26 <107043479+boris-faniuk-n26@users.noreply.github.com>
2024-06-12 10:12:45 +0200

    Verifying seen exceptions v2

--- src/test/java/org/junitpioneer/testkit/ExecutionResults.java
+++ src/test/java/org/junitpioneer/testkit/ExecutionResults.java
@@ -10,12 +10,13 @@
 
 package org.junitpioneer.testkit;
 
-import java.util.*;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.StreamSupport;
 
 import org.junit.platform.engine.DiscoverySelector;
-import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.engine.discovery.DiscoverySelectors;
 import org.junit.platform.testkit.engine.EngineExecutionResults;
 import org.junit.platform.testkit.engine.EngineTestKit;
