f550e0b1c87f0190ce7ac8811b67f107539612b9
Fanon Jupkwo <jupsfan@gmail.com>
2024-07-16 10:47:30 +0100

    trying to correct package-info.java formatting

--- src/main/java/org/junitpioneer/jupiter/displaynamegenerator/package-info.java
+++ src/main/java/org/junitpioneer/jupiter/displaynamegenerator/package-info.java
@@ -1,6 +1,5 @@
 /**
- * This package contains a custom {@linkplain org.junit.jupiter.api.DisplayNameGenerator }
- * called {@linkplain org.junitpioneer.jupiter.displaynamegenerator.ReplaceCamelCaseAndUnderscoreAndNumber }
+ * This package contains a custom {@linkplain org.junit.jupiter.api.DisplayNameGenerator } called {@linkplain org.junitpioneer.jupiter.displaynamegenerator.ReplaceCamelCaseAndUnderscoreAndNumber }
  */
 
 package org.junitpioneer.jupiter.displaynamegenerator;
