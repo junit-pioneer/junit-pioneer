98f385978934f8a7aa7c57cb475fb6698e729dee
Johnny Lim <izeye@naver.com>
2025-01-31 20:07:50 +0900

    Update fail date for FailAtExtensionDemo

--- src/demo/java/org/junitpioneer/jupiter/FailAtExtensionDemo.java
+++ src/demo/java/org/junitpioneer/jupiter/FailAtExtensionDemo.java
@@ -17,28 +17,28 @@ public class FailAtExtensionDemo {
 
 	// tag::fail_at_simple[]
 	@Test
-	@FailAt(date = "2025-01-01")
+	@FailAt(date = "2050-01-01")
 	void test() {
-		// Test will fail as soon as 1st of January 2025 is reached.
+		// Test will fail as soon as 1st of January 2050 is reached.
 	}
 	// end::fail_at_simple[]
 
 	// tag::fail_at_with_reason[]
 	@Test
-	@FailAt(reason = "We are not allowed anymore", date = "2025-01-01")
+	@FailAt(reason = "We are not allowed anymore", date = "2050-01-01")
 	void testWithReason() {
-		// Test will fail with the given reason as soon as 1st of January 2025 is reached.
+		// Test will fail with the given reason as soon as 1st of January 2050 is reached.
 	}
 	// end::fail_at_with_reason[]
 
 	@Nested
 	// tag::fail_at_at_class_level[]
-	@FailAt(date = "2025-01-01")
+	@FailAt(date = "2050-01-01")
 	class TestClass {
 
 		@Test
 		void test() {
-			// Test will fail as soon as 1st of January 2025 is reached.
+			// Test will fail as soon as 1st of January 2050 is reached.
 		}
 
 	}
