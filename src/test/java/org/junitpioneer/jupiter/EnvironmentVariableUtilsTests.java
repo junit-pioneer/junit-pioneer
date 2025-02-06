4ecc3ef10be5b954e20a82d25bf3b2d1d9696081
Verhas Mihaly <mihaly.verhas@intuitech.studio>
2025-02-06 13:11:27 +0100

    Unused imports

--- src/test/java/org/junitpioneer/jupiter/EnvironmentVariableUtilsTests.java
+++ src/test/java/org/junitpioneer/jupiter/EnvironmentVariableUtilsTests.java
@@ -11,14 +11,11 @@
 package org.junitpioneer.jupiter;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Map;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("JUnitPioneer system environment utilities")
