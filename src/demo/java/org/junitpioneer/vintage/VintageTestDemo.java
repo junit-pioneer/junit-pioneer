897d97975e3000ec46d6cb8f1b59facba28f63c5
Mihály Verhás <misi.verhas@gmail.com>
2026-09-17 21:37:27 +0200

    Add comment to explain why @Deprecated was added

--- src/demo/java/org/junitpioneer/vintage/VintageTestDemo.java
+++ src/demo/java/org/junitpioneer/vintage/VintageTestDemo.java
@@ -12,6 +12,8 @@ package org.junitpioneer.vintage;
 
 import java.util.List;
 
+// This is intentional, JUnit 4 -> 5+ migration annotation is @Deprecated by default
+// Marked here to prevent warnings during build
 @Deprecated
 public class VintageTestDemo {
 
